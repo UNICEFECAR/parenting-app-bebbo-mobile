249619326db5fcf1be58c2ad960c3f8cfa9bb7b3
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-01 17:58:30 +0530

    added app theme
    added app theme

---
+++ app/styles/theme.d.ts
@@ -0,0 +1,25 @@
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+
+    colors: {
+        PRIMARY_COLOR: string,
+        PRIMARY_TINTCOLOR: string,
+        SECONDARY_COLOR: string,
+        SECONDARY_TINTCOLOR: string,
+        CHILDDEVELOPMENT_COLOR: string,
+        CHILDDEVELOPMENT_TINTCOLOR: string,
+        VACCINATION_COLOR: string,
+        VACCINATION_TINTCOLOR: string,
+        HEALTHCHECKUP_COLOR: string,
+        HEALTHCHECKUP_TINTCOLOR: string,
+        CHILDGROWTH_COLOR: string,
+        CHILDGROWTH_TINTCOLOR: string,
+        ACTIVITIES_COLOR: string,
+        ACTIVITIES_TINTCOLOR: string,
+        ARTICLES_COLOR:string,
+        ARTICLES_TINTCOLOR: string,
+    };
+  }
