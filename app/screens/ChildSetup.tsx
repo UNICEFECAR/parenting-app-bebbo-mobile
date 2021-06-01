a050315dcf3592add97a4431023c59d75cab8b6f
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-01 15:26:01 +0530

    component files created
    component files created for screens and blank screens for aboutus and childprofile added,
    upgrades to navigator and drawer

--- app/screens/ChildSetup.tsx
+++ app/screens/ChildSetup.tsx
@@ -1,6 +1,7 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text, Button} from 'react-native';
+import ChildDate from '../components/ChildDate';
 import {RootStackParamList} from '../navigation/types';
 import { Header, HeaderText } from '../styles/style';
 type ChildSetupNavigationProp = StackNavigationProp<
@@ -17,6 +18,7 @@ const ChildSetup = ({navigation}: Props) => {
         <Header>
           <HeaderText>Please take a moment to personalize your app</HeaderText>
         </Header>
+        <ChildDate/>
         <Text>Relationship with child</Text>
       <Button
         title="Continue & Go to Home"
--- app/screens/home/SettingScreen.tsx
+++ app/screens/home/SettingScreen.tsx
@@ -1,20 +1,30 @@
-import {DrawerActions} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
+import { DrawerActions } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import {Text, Button} from 'react-native';
-import {HomeDrawerNavigatorStackParamList} from '../../navigation/types';
-import {Container} from '../../styles/style';
+import { useTranslation } from 'react-i18next';
+import { Text, Button, View, Pressable } from 'react-native';
+import { HomeDrawerNavigatorStackParamList } from '../../navigation/types';
+import { ButtonText, Container } from '../../styles/style';
 
 type SettingScreenNavigationProp =
   StackNavigationProp<HomeDrawerNavigatorStackParamList>;
 type Props = {
   navigation: SettingScreenNavigationProp;
 };
+
 const SettingScreen = (props: any) => {
+  const { t } = useTranslation();
   return (
     <>
       <Container>
         <Text>Settings screen</Text>
+        <View style={{ flex: 1 }}>
+          <Pressable
+            style={{ padding: 10 }}
+            onPress={() => props.navigation.navigate('CountrySelection')}>
+            <ButtonText>{t('editCountryLang')}</ButtonText>
+          </Pressable>
+        </View>
         <Button
           title="Toggle"
           onPress={() =>
