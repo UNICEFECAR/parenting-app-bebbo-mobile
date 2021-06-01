de31221810e37e37b443314f7b075fdc9c12c393
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-01 17:58:30 +0530

    added app theme
    added app theme

---
+++ app/styles/theme.ts
@@ -0,0 +1,23 @@
+
+import { DefaultTheme } from "styled-components";
+
+export const appTheme: DefaultTheme = {
+    colors: {
+        PRIMARY_COLOR: '#2B2F84',
+        PRIMARY_TINTCOLOR: '#8CAEE4',
+        SECONDARY_COLOR: '#00AEEF',
+        SECONDARY_TINTCOLOR: '#B3E7FA',
+        CHILDDEVELOPMENT_COLOR: '#FFC20E',
+        CHILDDEVELOPMENT_TINTCOLOR: '#FFEDB7',
+        VACCINATION_COLOR: '#00A8A8',
+        VACCINATION_TINTCOLOR: '#99DCDC',
+        HEALTHCHECKUP_COLOR: '#F6A7D7',
+        HEALTHCHECKUP_TINTCOLOR: '#FCE5F3',
+        CHILDGROWTH_COLOR: '#AB8AD5',
+        CHILDGROWTH_TINTCOLOR: '#D5C4EA',
+        ACTIVITIES_COLOR: '#0FD87E',
+        ACTIVITIES_TINTCOLOR: '#CFF7E5',
+        ARTICLES_COLOR: '#FF8D6B',
+        ARTICLES_TINTCOLOR: '#FFD2C4',
+    }
+};
