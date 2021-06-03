5a2c987930e67da9fedc055afecdb8bc4c8d8b99
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-03 13:14:37 +0530

    added header for tab screen

--- app/screens/home/bottomTabs/Home.tsx
+++ app/screens/home/bottomTabs/Home.tsx
@@ -1,50 +1,57 @@
-import {DrawerActions} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
+import { DrawerActions } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import {Text,View, Button, ScrollView} from 'react-native';
-import {HomeDrawerNavigatorStackParamList} from '../../../navigation/types';
+import { Text, View, Button, ScrollView } from 'react-native';
+import TabScreenHeader from '../../../components/TabScreenHeader';
+import { HomeDrawerNavigatorStackParamList } from '../../../navigation/types';
 type HomeNavigationProp = StackNavigationProp<HomeDrawerNavigatorStackParamList>;
 type Props = {
   navigation: HomeNavigationProp;
 };
-const Home = ({navigation}: Props) => {
+const Home = ({ navigation }: Props) => {
   return (
     <>
-      <ScrollView>
-        <Text>Home screen</Text>
-        <Button
-          title="Toggle"
-          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-        />
-        <View>
-          <Text>Your Child's Development</Text>
+      <View style={{
+        flexDirection: 'column',
+        flex: 1,
+      }}>
+          <TabScreenHeader />
+        <ScrollView style={{flex:4}}>
+          <Text>Home screen</Text>
           <Button
-          title="Read more"
-          onPress={() => {}}
-        />
-        </View>
-        <View>
-          <Text>Daily Reads</Text>
-        </View>
-        <View>
-        <Text>Your Child Milestones</Text>
-        </View>
-        <View>
-        <Text>Playing together</Text>
-        </View>
-        <View>
-        <Text>Expert Advice And Articles</Text>
-        </View>
-        <View>
-        <Text>Tools</Text>
-        </View>
-        <View>
-        <Text>How is your Parenting Experience</Text>
-        <Button
-          title="Take a Survey"
-          onPress={() => {}}/>
-        </View>
-      </ScrollView>
+            title="Toggle"
+            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+          />
+          <View>
+            <Text>Your Child's Development</Text>
+            <Button
+              title="Read more"
+              onPress={() => { }}
+            />
+          </View>
+          <View>
+            <Text>Daily Reads</Text>
+          </View>
+          <View>
+            <Text>Your Child Milestones</Text>
+          </View>
+          <View>
+            <Text>Playing together</Text>
+          </View>
+          <View>
+            <Text>Expert Advice And Articles</Text>
+          </View>
+          <View>
+            <Text>Tools</Text>
+          </View>
+          <View>
+            <Text>How is your Parenting Experience</Text>
+            <Button
+              title="Take a Survey"
+              onPress={() => { }} />
+          </View>
+        </ScrollView>
+      </View>
     </>
   );
 };
