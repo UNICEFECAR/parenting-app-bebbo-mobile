b80882f418af125869421afc5ae98067aded33d7
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-09 16:19:18 +0530

    walkthrough and homescreen updated
    SVG library ,linear gradient library added

--- app/components/homeScreen/Tools.tsx
+++ app/components/homeScreen/Tools.tsx
@@ -1,26 +1,62 @@
 import { DrawerActions } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import VectorImage from 'react-native-vector-image';
+import styled from 'styled-components/native';
 import { Header3Text } from '../../styles/style';
 
-
+const ContainerView = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 15px;
+  margin-top: 10px;
+  background-color: ${props => props.theme.colors.SECONDARY_TINTCOLOR};
+`;
 const Tools = ({ navigation }: Props) => {
-    return (
-        <>
-            <View style={{padding:10}}>
-            <View>
-            <Text>Tools</Text>
-            <View style={{ flex: 1, flexDirection: "row", height: 150 }}>
-              <View style={{ flex: 1, backgroundColor: "orange" }} />
-              <View style={{ flex: 1, backgroundColor:'purple' }} />
-              <View style={{ flex: 1, backgroundColor: "orange" }} />
-              <View style={{ flex: 1,  backgroundColor:'purple' }} />
+  return (
+    <>
+      <ContainerView>
+        <View>
+          <Text>Tools</Text>
+          <ScrollView style={{ flex: 1, flexDirection: "row" }} horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={styles.item} >
+              <VectorImage source={require('../../assets/svg/ic_vaccination_color.svg')} />
+              <Text style={styles.title}>Vaccination</Text>
             </View>
-          </View>
+            <View style={styles.item} >
+              <VectorImage source={require('../../assets/svg/ic_health_color.svg')} />
+              <Text style={styles.title}>Health checkup</Text>
             </View>
-        </>
-    );
+            <View style={styles.item}>
+              <VectorImage source={require('../../assets/svg/ic_growth_color.svg')} />
+              <Text style={styles.title}>Child Growth</Text>
+            </View>
+          </ScrollView>
+        </View>
+      </ContainerView>
+    </>
+  );
 };
 
 export default Tools;
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    margin: 3,
+    padding: 10,
+    // width: 200,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 16,
+    padding: 5,
+    width: 100,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+})
--- app/screens/home/bottomTabs/SupportChat.tsx
+++ app/screens/home/bottomTabs/SupportChat.tsx
@@ -1,17 +1,18 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FocusAwareStatusBar from '@components/FocusAwareStatusBar';
 import TabScreenHeader from '@components/TabScreenHeader';
 import { HomeDrawerNavigatorStackParamList } from '../../../navigation/types';
-
+import { ThemeContext } from 'styled-components';
 type SupportChatNavigationProp = StackNavigationProp<HomeDrawerNavigatorStackParamList>;
 type Props = {
   navigation: SupportChatNavigationProp;
 };
-const headerColor="blue";
 const SupportChat = ({ navigation }: Props) => {
+  const themeContext = useContext(ThemeContext);
+  const headerColor=themeContext.colors.PRIMARY_COLOR;
   return (
     <>
      <SafeAreaView style={{flex:1}}>
