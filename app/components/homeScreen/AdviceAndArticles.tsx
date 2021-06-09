773eaf12a3925d850b36f57d33fb7899333bb54d
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-09 16:19:18 +0530

    walkthrough and homescreen updated
    SVG library ,linear gradient library added

--- app/components/homeScreen/AdviceAndArticles.tsx
+++ app/components/homeScreen/AdviceAndArticles.tsx
@@ -1,26 +1,75 @@
 import { DrawerActions } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import VectorImage from 'react-native-vector-image';
+import styled from 'styled-components/native';
 import { Header3Text } from '../../styles/style';
 
-
+const ContainerView = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 15px;
+  margin-top: 10px;
+  background-color: ${props => props.theme.colors.ARTICLES_TINTCOLOR};
+`;
 const AdviceAndArticles = ({ navigation }: Props) => {
-    return (
-        <>
-            <View style={{padding:10}}>
-            <View>
-            <Text>Expert Advice And Articles</Text>
-            <View style={{ flex: 1, flexDirection: "row", height: 150 }}>
-              <View style={{ flex: 1, backgroundColor: "orange" }} />
-              <View style={{ flex: 1, backgroundColor:'purple' }} />
-              <View style={{ flex: 1, backgroundColor: "orange" }} />
-              <View style={{ flex: 1,  backgroundColor:'purple' }} />
+  return (
+    <>
+      <ContainerView>
+        <View>
+          <Text>Expert Advice And Articles</Text>
+
+          <ScrollView style={{ flex: 1, flexDirection: "row" }} horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={styles.item} >
+              <VectorImage source={require('../../assets/svg/ic_art_play.svg')} />
+              <Text style={styles.title}>Playing and Learning</Text>
             </View>
-          </View>
+            <View style={styles.item} >
+              <VectorImage source={require('../../assets/svg/ic_art_health.svg')} />
+              <Text style={styles.title}>Health and Wellbeing</Text>
             </View>
-        </>
-    );
+            <View style={styles.item}>
+              <VectorImage source={require('../../assets/svg/ic_art_safety.svg')} />
+              <Text style={styles.title}>Safety and Protection</Text>
+            </View>
+            <View style={styles.item}>
+              <VectorImage source={require('../../assets/svg/ic_art_responsive.svg')} />
+              <Text style={styles.title}>Responsive Parenting</Text>
+            </View>
+            <View style={styles.item}>
+              <VectorImage source={require('../../assets/svg/ic_art_parenting.svg')} />
+              <Text style={styles.title}>Responsive Parenting</Text>
+            </View>
+            <View style={styles.item}>
+              <VectorImage source={require('../../assets/svg/ic_art_nutrition.svg')} />
+              <Text style={styles.title}>Responsive Parenting</Text>
+            </View>
+          </ScrollView>
+        </View>
+      </ContainerView>
+    </>
+  );
 };
 
 export default AdviceAndArticles;
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    margin: 3,
+    padding: 10,
+    // width: 200,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 16,
+    padding: 5,
+    width: 100,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+})
--- app/screens/home/SettingScreen.tsx
+++ app/screens/home/SettingScreen.tsx
@@ -1,8 +1,10 @@
 import { DrawerActions } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Text, Button, View, Pressable } from 'react-native';
+import { Switch } from 'react-native-gesture-handler';
+import VectorImage from 'react-native-vector-image';
 import { HomeDrawerNavigatorStackParamList } from '../../navigation/types';
 import { ButtonText, Container } from '../../styles/style';
 
@@ -14,11 +16,22 @@ type Props = {
 
 const SettingScreen = (props: any) => {
   const { t } = useTranslation();
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <>
       <Container>
         <Text>Settings screen</Text>
         <View style={{ flex: 1 }}>
+       
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+
           <Pressable
             style={{ padding: 10 }}
             onPress={() => {
@@ -29,6 +42,8 @@ const SettingScreen = (props: any) => {
               }}>
             <ButtonText>{t('editCountryLang')}</ButtonText>
           </Pressable>
+
+          <VectorImage source={require('../../assets/svg/ic_gdrive.svg')} />
         </View>
         <Button
           title="Toggle"
