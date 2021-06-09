75c887389b8430a1b4892b18e8e9931fddccb44a
Kalyani790 <kalyani.kaushik@datamatics.com>
2021-06-09 15:37:27 +0530

    Image download and show from local in sponsars module.
    1.Image download and show from local in sponsars module.

--- app/types/apiConstants.ts
+++ app/types/apiConstants.ts
@@ -1,5 +1,6 @@
 
-import {apiUrlDevelop} from "@env";
+//import {apiUrlDevelop} from "@env";
+const apiUrlDevelop = 'http://parentbuddy2fz6bm64mba.devcloud.acquia-sites.com/api';
 export const finalUrl = (apiEndpoint:string,selectedCountry: number | undefined,selectedLang: string)=>{
     console.log("in finalurl");
     if(apiEndpoint==appConfig.sponsors){
