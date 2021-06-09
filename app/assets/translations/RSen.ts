4b820a21a78a263c01f170d66255a7f522c47d02
NehaRuparelgit <neha.ruparel@datamatics.com>
2021-06-09 10:16:27 +0530

    dynamic localisation
    - Added other new languages as requested.

---
+++ app/assets/translations/RSen.ts
@@ -0,0 +1,24 @@
+const RSen = {
+    translation: {
+        countryLangSelection: 'You have selected country and language',
+        choiceVerifiy: 'Please verify your choice',
+        editCountryLang: 'Edit',
+        continueCountryLang: 'Continue',
+        selectYourCountry: 'Select your country',
+        selectYourLang: 'Select your language',
+        doneCountryLang: 'Done',
+        Walkthroughscreen: 'Walkthrough screen',
+        goToLanguageSelection:'Go to LanguageSelection',
+        goToConfirm:'Go to Confirm',
+        checkonce:'Check once before proceeding',
+        country:'Country',
+        language:'Language',
+        termsandconditionheader:'Terms and Conditions',
+        tAndCCheckbox1:'I have read, I understand and accept Terms and Conditions of the application',
+        tAndCCheckbox2:'I have read, I understand and accept the ',
+        tAndCPrivacyPolicy:'Privacy Policy of the application',
+        tAndCCheckbox3:'I accept that the anonymized data on my use of the app will be used to inform improvements of the app (it is not mandatory)',
+        privacyPolicyTitle:'Privacy Policy',
+    }
+}
+export default RSen;
