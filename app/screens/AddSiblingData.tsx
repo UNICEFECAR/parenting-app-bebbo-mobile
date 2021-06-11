bd2a00d31a99cf4d80ea6b16ab47eb07ea1c6031
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-11 16:31:52 +0530

    add sibling data and chilssetuplist inline style added
    add sibling data and chilssetuplist inline style added

--- app/screens/AddSiblingData.tsx
+++ app/screens/AddSiblingData.tsx
@@ -1,12 +1,21 @@
-
-import { StackNavigationProp } from '@react-navigation/stack';
-import React, { createRef, useState } from 'react';
-import { View, Text, Button, StyleSheet, Pressable } from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {createRef, useState} from 'react';
+import {View, Text, Button, StyleSheet, Pressable} from 'react-native';
 import ChildDate from '@components/ChildDate';
-import { RootStackParamList } from '../navigation/types';
-import { Header, Container, HeaderText, Header2Text } from '../styles/style';
-
-
+import {RootStackParamList} from '../navigation/types';
+import {
+  Header,
+  Container,
+  HeaderText,
+  Header2Text,
+  ButtonText,
+} from '../styles/style';
+import OnboardingContainer from '@components/shared/OnboardingContainer';
+import {ButtonPrimary} from '@components/shared/ButtonGlobal';
+import Icon from '@components/shared/Icon';
+import OnboardingHeading from '@components/shared/OnboardingHeading';
+import {ChildCenterView} from '@components/shared/ChildSetupStyle';
+import { Heading1Centerw } from '../styles/typography';
 type ChildSetupNavigationProp = StackNavigationProp<
   RootStackParamList,
   'HomeDrawerNavigator'
@@ -16,26 +25,38 @@ type Props = {
   navigation: ChildSetupNavigationProp;
 };
 
-
-const AddSiblingData = ({ navigation }: Props) => {
-
+const AddSiblingData = ({navigation}: Props) => {
   return (
     <>
-      <Container>
+      <OnboardingContainer>
         <View>
-
-          <Header>
-            <HeaderText>Add Brother or Sister</HeaderText>
-          </Header>
+          <OnboardingHeading>
+            <ChildCenterView style={{flexDirection: 'row',justifyContent:'space-between'}}>
+              <Heading1Centerw>Add Brother or Sister</Heading1Centerw>
+              <Pressable
+                onPress={() => {
+                  navigation.goBack();
+                }}>
+                <Icon name="ic_close" size={20} color="#FFF" />
+              </Pressable>
+            </ChildCenterView>
+          </OnboardingHeading>
           <ChildDate />
-          
-        </View>
-          <Button
-            title="Save Data"
-            onPress={() => navigation.navigate('ChildSetupList')}
-          />
 
-      </Container>
+          <View style={{width: '100%', marginTop: 30}}>
+            <ButtonPrimary
+              onPress={() => {
+                // navigation.reset({
+                //   index: 0,
+                //   routes: [{name: 'HomeDrawerNavigator'}],
+                // })
+                navigation.navigate('ChildSetupList');
+              }}>
+              <ButtonText>Continue</ButtonText>
+            </ButtonPrimary>
+          </View>
+        </View>
+      </OnboardingContainer>
     </>
   );
 };
