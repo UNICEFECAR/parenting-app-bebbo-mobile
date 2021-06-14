019b3bde90571fed7892150bfe0c3c77c3cf45da
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-14 16:48:54 +0530

    formatter files

--- app/screens/localization/LanguageSelection.tsx
+++ app/screens/localization/LanguageSelection.tsx
@@ -1,16 +1,22 @@
-import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useEffect, useState } from 'react';
-import { FlatList} from 'react-native';
-import { SelectionView } from '../../styles/style';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
+import {SelectionView} from '../../styles/style';
 
-import { LocalizationStackParamList } from '../../navigation/types';
+import {LocalizationStackParamList} from '../../navigation/types';
 import LanguageItem from '@components/LanguageItem';
-import { useAppSelector } from '../../../App';
+import {useAppSelector} from '../../../App';
 import Icon from '@components/shared/Icon';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import OnboardingStyle from '@components/shared/OnboardingStyle';
 import OnboardingContainer from '@components/shared/OnboardingContainer';
-import { BtnMultiple, ButtonSection, ButtonviewClick, ButtonviewNext, ButtonviewPrevious } from '@components/shared/ButtonView';
+import {
+  BtnMultiple,
+  ButtonSection,
+  ButtonviewClick,
+  ButtonviewNext,
+  ButtonviewPrevious,
+} from '@components/shared/ButtonView';
 type LanguageSelectionNavigationProp = StackNavigationProp<
   LocalizationStackParamList,
   'CountryLanguageConfirmation'
@@ -18,20 +24,22 @@ type LanguageSelectionNavigationProp = StackNavigationProp<
 type Props = {
   navigation: LanguageSelectionNavigationProp;
 };
-const LanguageSelection = ({ route, navigation }: Props) => {
+const LanguageSelection = ({route, navigation}: Props) => {
   const [language, setLanguage] = useState();
-  const { country } = route.params;
+  const {country} = route.params;
   // console.log(country);
   const languages = country.languages;
-  const { t } = useTranslation();
+  const {t} = useTranslation();
   const languageCode = useAppSelector(
     (state: any) => state.selectedCountry.languageCode,
   );
   useEffect(() => {
-    const selectedLanguage = languages.find(lang => lang.languageCode === languageCode)
+    const selectedLanguage = languages.find(
+      (lang) => lang.languageCode === languageCode,
+    );
     setLanguage(selectedLanguage);
   }, []);
-  const renderItem = ({ item }: any) => (
+  const renderItem = ({item}: any) => (
     <LanguageItem
       item={item}
       currentItem={language}
@@ -40,12 +48,11 @@ const LanguageSelection = ({ route, navigation }: Props) => {
   );
   return (
     <>
-
-
       <OnboardingContainer>
-        <OnboardingStyle title={t('selectYourCountry').toString()} iconname='ic_act_language' />
-
-
+        <OnboardingStyle
+          title={t('selectYourCountry').toString()}
+          iconname="ic_act_language"
+        />
 
         <SelectionView>
           <FlatList
@@ -55,31 +62,26 @@ const LanguageSelection = ({ route, navigation }: Props) => {
           />
         </SelectionView>
         {language ? (
-
-
           <BtnMultiple>
             <ButtonviewPrevious>
-              <ButtonviewClick onPress={() =>
-                navigation.goBack()
-              }>
+              <ButtonviewClick onPress={() => navigation.goBack()}>
                 <Icon name="ic_angle_left" size={32} color="#000" />
                 {/* <Image style={{width:50,height:50}} source={ require( '@assets/ic_prev_arrow.png') } /> */}
               </ButtonviewClick>
             </ButtonviewPrevious>
             <ButtonviewNext>
-              <ButtonviewClick onPress={() =>
-                navigation.navigate('CountryLanguageConfirmation', {
-                  country,
-                  language,
-                })
-              }>
+              <ButtonviewClick
+                onPress={() =>
+                  navigation.navigate('CountryLanguageConfirmation', {
+                    country,
+                    language,
+                  })
+                }>
                 {/* <ButtonText>{t('goToConfirm')}</ButtonText> */}
                 <Icon name="ic_angle_right" size={32} color="#000" />
               </ButtonviewClick>
             </ButtonviewNext>
           </BtnMultiple>
-
-
         ) : null}
       </OnboardingContainer>
     </>
