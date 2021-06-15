4bcea9f8afd3efb4fe5876e766c215c798300de5
prashant-acharekar <prashant.acharekar@datamatics.com>
2021-06-15 13:28:25 +0530

    Minor UI Changes fixed in the Walkthrough, Terms, Child Setup Screens.

---
+++ app/components/shared/FormPrematureContainer.tsx
@@ -0,0 +1,25 @@
+
+import styled from 'styled-components/native';
+const FormPrematureContainer = styled.View`
+  width: 100%;
+  height:40px;
+  
+  flex-direction:row;
+  align-Items:center;
+  margin-bottom:20px;
+`;
+
+export const FormOuterCheckbox = styled.Text`
+margin-right:10px;
+align-items:flex-start;
+
+`
+
+export const FormCheckboxLabel = styled.Text`
+flex-direction:row;
+
+`
+
+
+
+export default FormPrematureContainer;
