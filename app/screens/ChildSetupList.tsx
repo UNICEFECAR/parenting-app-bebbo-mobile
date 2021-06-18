e31de20d32129569e9801d83c80cb4b7d117ea81
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-18 15:10:46 +0530

    update to last merge

--- app/screens/ChildSetupList.tsx
+++ app/screens/ChildSetupList.tsx
@@ -1,22 +1,36 @@
-
-import { ChildCenterView, ChildColArea1, ChildColArea2, ChildContentArea, ChildListingArea, ChildListingBox, ChildListTitle, TitleLinkSm } from '@components/shared/ChildSetupStyle';
-import Icon from '@components/shared/Icon';
+import {
+  ButtonLinkText,
+  ButtonPrimary,
+  ButtonRow,
+  ButtonText
+} from '@components/shared/ButtonGlobal';
+import {
+  ChildCenterView,
+  ChildColArea1,
+  ChildColArea2,
+  ChildContentArea,
+  ChildListingArea,
+  ChildListingBox,
+  ChildListTitle,
+  TitleLinkSm
+} from '@components/shared/ChildSetupStyle';
+import Icon, { OuterIconLeft, OuterIconRow } from '@components/shared/Icon';
 import OnboardingContainer from '@components/shared/OnboardingContainer';
 import OnboardingHeading from '@components/shared/OnboardingHeading';
+import { RootStackParamList } from '@navigation/types';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import {View, Text, Pressable} from 'react-native';
-import {useAppDispatch} from '../../App';
-import {RootStackParamList} from '../navigation/types';
-import {appConfig} from '../types/apiConstants';
-import {Heading3Centerw, Heading1Centerw, ShiftFromBottom20} from '../styles/typography';
-import Icon, { OuterIconLeft, OuterIconRow } from '@components/shared/Icon';
-import { ButtonContainer,ButtonPrimary,ButtonText,ButtonLinkText, ButtonRow,ButtonText } from '@components/shared/ButtonGlobal';
+import { Pressable, Text } from 'react-native';
+import {
+  Heading1Centerw,
+  Heading3Centerw,
+  ShiftFromBottom20
+} from '../styles/typography';
+import { appConfig } from '../types/apiConstants';
 type ChildSetupNavigationProp = StackNavigationProp<
   RootStackParamList,
   'AddSiblingDataScreen'
 >;
-
 type Props = {
   navigation: ChildSetupNavigationProp;
 };
@@ -35,7 +49,12 @@ const ChildSetupList = ({navigation}: Props) => {
       },
       saveinDB: true,
     },
-    {apiEndpoint:appConfig.taxonomies,method:'get',postdata:{},saveinDB:true}
+    {
+      apiEndpoint: appConfig.taxonomies,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
     // {apiEndpoint:appConfig.basicPages,method:'get',postdata:{},saveinDB:true}
   ];
   const childSetup = () => {
@@ -58,62 +77,62 @@ const ChildSetupList = ({navigation}: Props) => {
   return (
     <>
       <OnboardingContainer>
-        
         <OnboardingHeading>
-            <ChildCenterView>
-              <Heading1Centerw>Awesome! Your child profile has been created !</Heading1Centerw>
-              <Heading3Centerw>Please review your child details before proceeding!</Heading3Centerw>
-            </ChildCenterView>
+          <ChildCenterView>
+            <Heading1Centerw>
+              Awesome! Your child profile has been created !
+            </Heading1Centerw>
+            <Heading3Centerw>
+              Please review your child details before proceeding!
+            </Heading3Centerw>
+          </ChildCenterView>
         </OnboardingHeading>
         <ChildContentArea>
-            <ChildListingArea>
+          <ChildListingArea>
             <ChildListingBox>
-            <ChildColArea1>
-              <ChildListTitle>Child 1</ChildListTitle>
-              <Text>Born on 08 july 2020</Text>
-            </ChildColArea1>
-            <ChildColArea2>
-              <TitleLinkSm>Delete</TitleLinkSm>
-              <TitleLinkSm>Edit Profile</TitleLinkSm>
-            </ChildColArea2>
-          </ChildListingBox>
+              <ChildColArea1>
+                <ChildListTitle>Child 1</ChildListTitle>
+                <Text>Born on 08 july 2020</Text>
+              </ChildColArea1>
+              <ChildColArea2>
+                <TitleLinkSm>Delete</TitleLinkSm>
+                <TitleLinkSm>Edit Profile</TitleLinkSm>
+              </ChildColArea2>
+            </ChildListingBox>
 
-          <ChildListingBox>
-            <ChildColArea1>
-              <ChildListTitle>Child 2</ChildListTitle>
-              <Text>Born on 22 june 2018</Text>
-            </ChildColArea1>
-            <ChildColArea2>
-              <TitleLinkSm>Delete</TitleLinkSm>
-              <TitleLinkSm>Edit Profile</TitleLinkSm>
-            </ChildColArea2>
-          </ChildListingBox>
-
-            </ChildListingArea>
+            <ChildListingBox>
+              <ChildColArea1>
+                <ChildListTitle>Child 2</ChildListTitle>
+                <Text>Born on 22 june 2018</Text>
+              </ChildColArea1>
+              <ChildColArea2>
+                <TitleLinkSm>Delete</TitleLinkSm>
+                <TitleLinkSm>Edit Profile</TitleLinkSm>
+              </ChildColArea2>
+            </ChildListingBox>
+          </ChildListingArea>
         </ChildContentArea>
-
-        
 
         <ButtonRow>
           <ShiftFromBottom20>
-            <Pressable style ={{flexDirection:'row', justifyContent:'center',}} 
+            <Pressable
+              style={{flexDirection: 'row', justifyContent: 'center'}}
               onPress={() => navigation.navigate('AddSiblingDataScreen')}>
               <OuterIconRow>
-              <OuterIconLeft>
-              <Icon name="ic_plus" size={20} color="#FFF" />
-              </OuterIconLeft>
-              <ButtonLinkText>Add Sister or Brother</ButtonLinkText>
+                <OuterIconLeft>
+                  <Icon name="ic_plus" size={20} color="#FFF" />
+                </OuterIconLeft>
+                <ButtonLinkText>Add Sister or Brother</ButtonLinkText>
               </OuterIconRow>
             </Pressable>
-            </ShiftFromBottom20>
+          </ShiftFromBottom20>
 
-          <ButtonPrimary onPress={() => {
-                      childSetup();
-                    }}
-                  >
-                    <ButtonText>Continue</ButtonText>
-                  </ButtonPrimary>
-
+          <ButtonPrimary
+            onPress={() => {
+              childSetup();
+            }}>
+            <ButtonText>Continue</ButtonText>
+          </ButtonPrimary>
         </ButtonRow>
       </OnboardingContainer>
     </>
