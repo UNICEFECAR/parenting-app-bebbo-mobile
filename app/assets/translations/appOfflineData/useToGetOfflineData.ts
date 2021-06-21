883ddc943ec6b4db125890ea77b71aa51fc6d18e
NehaRuparelgit <neha.ruparel@datamatics.com>
2021-06-21 14:32:04 +0530

    Dynamic Article Listing screen
    - Added code for Dynamic article listing.
    - Image downlaod code added
    - Added loading component and called on screen for data load.

--- app/assets/translations/appOfflineData/useToGetOfflineData.ts
+++ app/assets/translations/appOfflineData/useToGetOfflineData.ts
@@ -1,17 +1,17 @@
 import { ObjectSchema } from "realm";
 import { dataRealmCommon } from "../../../database/dbquery/dataRealmCommon";
 
-const useToGetOfflineData = (languageCode:string,dispatch:any,SchemaToUse:ObjectSchema,SchemaEntity:any,jsonData:any,setAllHardcodedData:Function) => {
+const useToGetOfflineData = async (languageCode:string,dispatch:any,SchemaToUse:ObjectSchema,SchemaEntity:any,jsonData:any,setAllHardcodedData:Function) => {
     // const [taxonomyData,setTaxonomyData] = React.useState();
     console.log(languageCode,"languageCode in taxonomy file");
     //Get data from db and if it exists then use db data or use const data in this file.  
     let databaselistener:any;
-    async function fetchData() {
+    // async function fetchData() {
         let databaseData2 = await dataRealmCommon.getData<typeof SchemaEntity>(SchemaToUse);
-        console.log("db length--",databaseData2?.length);
+        console.log("  db length--",databaseData2?.length);
             if(databaseData2?.length > 0)
             {
-                dispatch(setAllHardcodedData(databaseData2));
+                // dispatch(setAllHardcodedData(databaseData2));
                 databaseData2.removeAllListeners();
                 databaselistener = databaseData2.addListener(() => {
                     console.log("in if listener called--",databaseData2);
@@ -30,9 +30,9 @@ const useToGetOfflineData = (languageCode:string,dispatch:any,SchemaToUse:Object
                     }
                 });
             }
-    }
-    fetchData()
-    return [];
+    // }
+    // fetchData()
+    return [databaseData2];
 }
 
   
