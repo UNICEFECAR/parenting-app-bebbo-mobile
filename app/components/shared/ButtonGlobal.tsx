45d80ba55d7c4ce560aded6af3bb080326c19b03
Kalyani790 <kalyani.kaushik@datamatics.com>
2021-06-22 11:16:42 +0530

    t&c validations
    Terms validations added
    apiconstant @env replaced with package due to error

--- app/components/shared/ButtonGlobal.tsx
+++ app/components/shared/ButtonGlobal.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components/native';
 
 /*Button Pressable */
 export const ButtonPrimary = styled.Pressable`
-background: ${props => props.theme.colors.SECONDARY_COLOR};
+background: ${props => props.disabled ? '#cccccc' : props.theme.colors.SECONDARY_COLOR};
+opacity: ${props => props.disabled ? 0.5 : 1};
 color: ${props => props.theme.colors.PRIMARY_TEXTCOLOR};
 padding: 14px 15px;
 width:100%;
