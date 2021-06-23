03bbfa39137e8614d070d60031ff50fb96dd3be2
prashant-acharekar <prashant.acharekar@datamatics.com>
2021-06-23 16:40:50 +0530

    manage profile screen UI changes

--- app/components/shared/ProfileListingStyle.tsx
+++ app/components/shared/ProfileListingStyle.tsx
@@ -11,6 +11,8 @@ flex-direction:row;
 align-items:center;
 `
 
+
+
 export const ProfileListViewSelected = styled(ProfileListView)`
 background-color: ${props => props.theme.colors.SECONDARY_COLOR};
 
@@ -60,6 +62,64 @@ align-items:flex-start;
 border-radius:4px;
 `
 
+
+export const ProfileLinkRow = styled.View`
+width:100%;
+  flex-direction:row;
+  margin:0;
+  padding: 10px;
+ margin-bottom:15px;
+`;
+
+export const ProfileLinkCol = styled.View`
+ 
+  align-items:center;
+  justify-content:flex-start;
+  flex:1;
+  padding: 0 15px;
+  flex-direction:column;
+`;
+export const ProfileContentView = styled.View`
+ width:100%;
+ align-items:center;
+ flex-direction:row;
+ flex:1;
+ 
+`;
+
+export const ParentRowView = styled.View`
+ flex-direction:column;
+ flex:1;
+ margin:10px 0 10px;
+`;
+export const ParentSection = styled.View`
+flex-direction:row;
+padding:5px 7px;
+`
+
+export const ParentLabel = styled.View`
+ flex:1;
+
+`;
+export const ParentData = styled.View`
+flex:2;
+
+`;
+
+export const ParentColView = styled.View`
+ 
+ 
+`;
+
+export const ParentListView = styled.View`
+padding:10px 15px;
+align-items:center;
+border-radius:4px;
+`
+
+export const ChildListScrollView = styled.View`
+height:350px;margin-top:15px;border-radius:4px;
+`
 
 /*Manage Profile Screen End*/
 
