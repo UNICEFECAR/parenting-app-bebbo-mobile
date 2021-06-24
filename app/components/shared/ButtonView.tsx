dcf8f296aaddcbbacf71b3a5171e9c7a46c1fb43
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-24 14:50:32 +0530

    update on style typos
    update on style typos

--- app/components/shared/ButtonView.tsx
+++ app/components/shared/ButtonView.tsx
@@ -1,55 +1,35 @@
-
 import styled from 'styled-components/native';
 export const ButtonviewNext = styled.Text`
-width:50px;height:50px;
-margin-top:30;
+  width: 50px;
+  height: 50px;
+  margin-top: 30;
 
-align-content:center;
-margin-bottom:15;
-border-radius:100px;
-text-align:center;
-background-color: ${props => props.theme.colors.SECONDARY_COLOR};
+  align-content: center;
+  margin-bottom: 15;
+  border-radius: 100px;
+  text-align: center;
+  background-color: ${(props) => props.theme.colors.SECONDARY_COLOR};
 `;
-
 
 export const ButtonviewPrevious = styled(ButtonviewNext)`
-background-color: ${props => props.theme.colors.SECONDARY_TINTCOLOR};
-justify-content:flex-start;
-
+  background-color: ${(props) => props.theme.colors.SECONDARY_TINTCOLOR};
+  justify-content: flex-start;
 `;
 
-
-
 export const ButtonviewClick = styled.Pressable`
-
-margin-top:10px;
-flex-direction:column;
-justify-content:center;
-height:50px;
-align-content:center
-
-`
-
-
+  margin-top: 10px;
+  flex-direction: column;
+  justify-content: center;
+  height: 50px;
+  align-content: center;
+`;
 
 export const ButtonSection = styled.View`
-flex-direction:row;
-justify-content:flex-end;
-width:100%;
-
-`
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 100%;
+`;
 
 export const BtnMultiple = styled(ButtonSection)`
-
-justify-content:space-between;
-
-`
-
-
-
-
-
-
-
-
-
+  justify-content: space-between;
+`;
