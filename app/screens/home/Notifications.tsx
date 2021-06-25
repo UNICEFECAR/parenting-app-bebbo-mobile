687dd739bc01beaf2c32345e5b25b6c17ea63dfe
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-25 09:52:25 +0530

    changes to iconconfig and notifficatons tsx
    changes to iconconfig and notifficatons tsx

--- app/screens/home/Notifications.tsx
+++ app/screens/home/Notifications.tsx
@@ -1,25 +1,53 @@
 import FocusAwareStatusBar from '@components/FocusAwareStatusBar';
 import NotificationsCategories from '@components/NotificationsCategories';
 import BurgerIcon from '@components/shared/BurgerIcon';
+import Icon from '@components/shared/Icon';
 import { HomeDrawerNavigatorStackParamList } from '@navigation/types';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Heading2w } from '@styles/typography';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { ThemeContext } from 'styled-components';
 type NotificationsNavigationProp =
   StackNavigationProp<HomeDrawerNavigatorStackParamList>;
 const Notifications = () => {
   const themeContext = useContext(ThemeContext);
   const primaryColor = themeContext.colors.PRIMARY_COLOR;
-  const { t } = useTranslation();
+  const {t} = useTranslation();
   const onchange = (selectedboxes) => {
     console.log(selectedboxes);
   };
+  const DATA = [
+    {
+      id: 0,
+      title: "You haven't update your child's growth details from last 1 month",
+      timeStamp: '10 minutes ago',
+      type: 'growth',
+    },
+    {
+      id: 2,
+      title:
+        "The next development milestones are set for jenny. Fill all the question to track you baby's milestones.",
+      timeStamp: '15 minutes ago',
+      type: 'development',
+    },
+    {
+      id: 3,
+      title: 'Reminder has been set for the vaccination',
+      timeStamp: '19 minutes ago',
+      type: 'vaccination',
+    },
+    {
+      id: 4,
+      title: "Update your child growth data to track baby's growth",
+      timeStamp: '20 minutes ago',
+      type: 'healthchkp',
+    },
+  ];
   return (
     <>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{flex: 1}}>
         <FocusAwareStatusBar animated={true} backgroundColor={primaryColor} />
         <View
           style={{
@@ -28,17 +56,44 @@ const Notifications = () => {
             backgroundColor: primaryColor,
             maxHeight: 50,
           }}>
-          <View style={{ flex: 1 }}>
+          <View style={{flex: 1}}>
             <BurgerIcon />
           </View>
-          <View style={{ flex: 5, padding: 8 }}>
+          <View style={{flex: 5, padding: 8}}>
             <Heading2w> {t('notiScreen.headerTitle')}</Heading2w>
           </View>
         </View>
 
         <NotificationsCategories onchange={onchange} />
+        <View style={{margin: 10}}>
+          {DATA.map((item, index) => {
+            return (
+              <View style={{flexDirection: 'row', padding: 10, margin: 10}}>
+                <Icon name={'ic_growth'} size={20} color="#000" />
+                <View style={{flexDirection: 'column'}}>
+                  <View>
+                    <Text>{item.title}</Text>
+                    <Text>{item.timeStamp}</Text>
+                  </View>
+                  <View
+                    style={{
+                      padding: 1,
+                      margin: 5,
+                      backgroundColor: '#000',
+                    }}></View>
+                </View>
+                <Icon
+                  style={{flex: 1, textAlign: 'right', alignSelf: 'center'}}
+                  name={'ic_kebabmenu'}
+                  size={20}
+                  color="#000"
+                />
+              </View>
+            );
+          })}
+        </View>
       </SafeAreaView>
     </>
-  )
-}
+  );
+};
 export default Notifications;
