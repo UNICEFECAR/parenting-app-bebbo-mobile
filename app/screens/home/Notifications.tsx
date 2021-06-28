f3d16edd393a546ffdc6bdae1c878ceb525251f2
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-28 10:59:44 +0530

    menupicker added for notifications
    menupicker added for notifications

--- app/screens/home/Notifications.tsx
+++ app/screens/home/Notifications.tsx
@@ -4,19 +4,31 @@ import BurgerIcon from '@components/shared/BurgerIcon';
 import Icon from '@components/shared/Icon';
 import { HomeDrawerNavigatorStackParamList } from '@navigation/types';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Heading2w } from '@styles/typography';
+import { Heading2w, Heading5Bold } from '@styles/typography';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView, Text, View } from 'react-native';
+import {
+  Menu,
+  MenuOption,
+  MenuOptions,
+  MenuTrigger,
+  renderers
+} from 'react-native-popup-menu';
 import { ThemeContext } from 'styled-components';
 type NotificationsNavigationProp =
   StackNavigationProp<HomeDrawerNavigatorStackParamList>;
 const Notifications = () => {
   const themeContext = useContext(ThemeContext);
   const primaryColor = themeContext.colors.PRIMARY_COLOR;
+  const primaryTintColor = themeContext.colors.PRIMARY_TINTCOLOR;
+  const growthColor = themeContext.colors.CHILDGROWTH_COLOR;
+  const vaccinationColor = themeContext.colors.VACCINATION_COLOR;
+  const hkColor = themeContext.colors.HEALTHCHECKUP_COLOR;
+  const cdColor = themeContext.colors.CHILDDEVELOPMENT_COLOR;
   const {t} = useTranslation();
   const onchange = (selectedboxes) => {
-   // console.log(selectedboxes);
+    // console.log(selectedboxes);
   };
   const DATA = [
     {
@@ -24,6 +36,7 @@ const Notifications = () => {
       title: "You haven't update your child's growth details from last 1 month",
       timeStamp: '10 minutes ago',
       type: 'growth',
+      bgColor: growthColor,
     },
     {
       id: 2,
@@ -31,23 +44,34 @@ const Notifications = () => {
         "The next development milestones are set for jenny. Fill all the question to track you baby's milestones.",
       timeStamp: '15 minutes ago',
       type: 'development',
+      bgColor: cdColor,
     },
     {
       id: 3,
       title: 'Reminder has been set for the vaccination',
       timeStamp: '19 minutes ago',
       type: 'vaccination',
+      bgColor: vaccinationColor,
     },
     {
       id: 4,
       title: "Update your child growth data to track baby's growth",
       timeStamp: '20 minutes ago',
       type: 'healthchkp',
+      bgColor: hkColor,
     },
   ];
-  const geticonname =(type:string)=>{
-   return type=="growth" ? ("ic_growth"): type=="development" ?  "ic_milestone":type=="vaccination" ?  "ic_vaccination":type=="healthchkp" ?  "ic_doctor_chk_up":"";
-  }
+  const geticonname = (type: string) => {
+    return type == 'growth'
+      ? 'ic_growth'
+      : type == 'development'
+      ? 'ic_milestone'
+      : type == 'vaccination'
+      ? 'ic_vaccination'
+      : type == 'healthchkp'
+      ? 'ic_doctor_chk_up'
+      : '';
+  };
   return (
     <>
       <SafeAreaView style={{flex: 1}}>
@@ -62,18 +86,43 @@ const Notifications = () => {
           <View style={{flex: 1}}>
             <BurgerIcon />
           </View>
-          <View style={{ flex: 5, padding: 8 }}>
+          <View style={{flex: 5, padding: 8}}>
             <Heading2w> {t('notiScreenheaderTitle')}</Heading2w>
+          </View>
+          <View style={{flex: 1,flexDirection:'row'}}>
+            <Icon name={'ic_sb_settings'} size={20} color="#FFF" />
+            <Icon name={'ic_trash'} size={20} color="#FFF" />
           </View>
         </View>
 
         <NotificationsCategories onchange={onchange} />
-        <View style={{margin: 10}}>
+        <View style={{marginVertical: 10}}>
           {DATA.map((item, index) => {
             return (
-              <View style={{flexDirection: 'row', padding: 10, margin: 10}}>
-                <Icon name={geticonname(item.type)} size={20} color="#000" style={{flex:1}}/>
-                <View style={{flexDirection: 'column',flex:5}}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  paddingVertical: 10,
+                  marginVertical: 10,
+                }}
+                key={index}>
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: 50,
+                    backgroundColor: item.bgColor,
+                    flex: 1,
+                  }}>
+                  <Icon
+                    name={geticonname(item.type)}
+                    size={20}
+                    color="#000"
+                    style={{padding: 15}}
+                  />
+                </View>
+
+                <View style={{flexDirection: 'column', flex: 5}}>
                   <View>
                     <Text>{item.title}</Text>
                     <Text>{item.timeStamp}</Text>
@@ -85,12 +134,49 @@ const Notifications = () => {
                       backgroundColor: '#000',
                     }}></View>
                 </View>
-                <Icon
-                  style={{flex: 1, textAlign: 'right', alignSelf: 'center'}}
-                  name={'ic_kebabmenu'}
-                  size={20}
-                  color="#000"
-                />
+                <Menu
+                  renderer={renderers.ContextMenu}
+                  style={{
+                    width: 50,
+                    height: 50,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onSelect={(value) =>
+                    console.log(`Selected number: ${value}`)
+                  }>
+                  <MenuTrigger>
+                    <Icon
+                      style={{
+                        flex: 1,
+                        textAlign: 'right',
+                        alignSelf: 'center',
+                      }}
+                      name={'ic_kebabmenu'}
+                      size={25}
+                      color="#000"
+                    />
+                  </MenuTrigger>
+                  <MenuOptions
+                    customStyles={{
+                      optionsContainer: {
+                        marginTop: 30,
+                        borderRadius: 10,
+                        backgroundColor: primaryTintColor,
+                      },
+                      optionWrapper: {
+                        borderBottomWidth: 1,
+                        padding: 15,
+                      },
+                    }}>
+                    <MenuOption value={1}>
+                      <Heading5Bold>Delete this notification</Heading5Bold>
+                    </MenuOption>
+                    <MenuOption value={2}>
+                      <Heading5Bold>Mark as Read</Heading5Bold>
+                    </MenuOption>
+                  </MenuOptions>
+                </Menu>
               </View>
             );
           })}
