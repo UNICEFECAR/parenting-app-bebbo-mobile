0c822bfccd8140ffcb8d46c06283909221b45e40
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-28 10:59:44 +0530

    menupicker added for notifications
    menupicker added for notifications

--- App.tsx
+++ App.tsx
@@ -7,26 +7,34 @@
 
 // declare const global: {HermesInternal: null | {}};
 
-import 'react-native-gesture-handler';
+import { Action, ThunkAction } from '@reduxjs/toolkit';
 import React from 'react';
-import {Pressable, SafeAreaView, Text, View} from 'react-native';
-import AppNavigation from './app/navigation/AppNavigation';
+import {
+  ActivityIndicator,
+  Pressable,
+  SafeAreaView,
+  Text,
+  View
+} from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
+import 'react-native-gesture-handler';
+import { MenuProvider } from 'react-native-popup-menu';
+import { enableScreens } from 'react-native-screens';
+import SplashScreen from 'react-native-splash-screen';
 import {
   Provider,
   TypedUseSelectorHook,
   useDispatch,
-  useSelector,
+  useSelector
 } from 'react-redux';
-import configureAppStore from './app/redux/store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {ActivityIndicator} from 'react-native';
-import {Action, ThunkAction} from '@reduxjs/toolkit';
+import { PersistGate } from 'redux-persist/integration/react';
 import persistStore from 'redux-persist/lib/persistStore';
+import { ThemeProvider } from 'styled-components/native';
 import './app/localization/initI18next';
+import AppNavigation from './app/navigation/AppNavigation';
+import configureAppStore from './app/redux/store';
+import { appTheme } from './app/styles/theme';
 
-import {ThemeProvider} from 'styled-components/native';
-import {appTheme} from './app/styles/theme';
-import ErrorBoundary from 'react-native-error-boundary'
 export const store = configureAppStore();
 export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
@@ -37,21 +45,22 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
-import { enableScreens } from 'react-native-screens';
-import ImageStorage from './app/downloadImages/ImageStorage';
-import { useNetInfo } from '@react-native-community/netinfo';
-import SplashScreen from 'react-native-splash-screen';
 enableScreens();
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-const CustomFallback = (props: { error: Error, resetError: Function }) => (
+const CustomFallback = (props: {error: Error; resetError: Function}) => (
   <View>
     <Text>Something happened!</Text>
     <Text>{props.error.toString()}</Text>
-    <Pressable onPress={()=>{props.resetError}} ><Text>{'Try again'} </Text></Pressable>
+    <Pressable
+      onPress={() => {
+        props.resetError;
+      }}>
+      <Text>{'Try again'} </Text>
+    </Pressable>
   </View>
-)
+);
 
 const App = () => {
   React.useEffect(() => {
@@ -59,19 +68,19 @@ const App = () => {
   });
   return (
     <ErrorBoundary FallbackComponent={CustomFallback}>
-    <ThemeProvider theme={appTheme}>
-   
-      <Provider store={store}>
-        <PersistGate
-          loading={<ActivityIndicator size="large" color="#0000ff" />}
-          persistor={persistor}>
-          <SafeAreaView style={{flex: 1}}>
-          <AppNavigation />
-          </SafeAreaView>
-        </PersistGate>
-      </Provider>
-   
-    </ThemeProvider>
+      <ThemeProvider theme={appTheme}>
+        <MenuProvider>
+          <Provider store={store}>
+            <PersistGate
+              loading={<ActivityIndicator size="large" color="#0000ff" />}
+              persistor={persistor}>
+              <SafeAreaView style={{flex: 1}}>
+                <AppNavigation />
+              </SafeAreaView>
+            </PersistGate>
+          </Provider>
+        </MenuProvider>
+      </ThemeProvider>
     </ErrorBoundary>
   );
 };
