d345aaa122b69e7f8855569cb4da1f622b694395
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-29 16:23:16 +0530

    vaccine main screen added
    Vaccine main screen added ,header for all screens added as per ui

---
+++ app/components/PreviousVaccines.tsx
@@ -0,0 +1,205 @@
+import { useNavigation } from '@react-navigation/native';
+import { Heading2, Heading5 } from '@styles/typography';
+import React, { useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { ThemeContext } from 'styled-components';
+import Icon from './shared/Icon';
+
+const PreviousVaccines = (props: any) => {
+  const {item, headerColor, backgroundColor} = props;
+  // console.log(item);
+  const {t} = useTranslation();
+  const navigation = useNavigation();
+  const [isOPen, setIsOPen] = useState<Boolean>(false);
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const themeContext = useContext(ThemeContext);
+  const actHeaderColor = themeContext.colors.ACTIVITIES_COLOR;
+  const actBackgroundColor = themeContext.colors.ACTIVITIES_TINTCOLOR;
+  const artHeaderColor = themeContext.colors.ARTICLES_COLOR;
+  const artBackgroundColor = themeContext.colors.ARTICLES_TINTCOLOR;
+  const gotoArticle = () => {
+    navigation.navigate('DetailsScreen', {
+      fromScreen: 'ChildDevelopment',
+      headerColor: artHeaderColor,
+      backgroundColor: artBackgroundColor,
+    });
+  };
+  return (
+    <>
+      <View style={styles.item}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            backgroundColor: backgroundColor,
+          }}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            {item.totalVc === item.doneVc ? (
+              <Text
+                style={{
+                  alignItems: 'center',
+                  lineHeight: 20,
+                  flexDirection: 'row',
+                  textAlign: 'center',
+                  width: 20,
+                  height: 20,
+                  borderRadius: 100,
+                  backgroundColor: 'green',
+                }}>
+                <Icon name="ic_tick" size={12} color="#FFF" />
+              </Text>
+            ) : (
+              <Icon
+                name="ic_incom"
+                size={20}
+                color="#FFF"
+                style={{backgroundColor: 'red', borderRadius: 150}}
+              />
+            )}
+          </View>
+          <Pressable
+            style={{
+              flexDirection: 'row',
+              flex: 7,
+            }}
+            onPress={() => {
+              setIsOPen(!isOPen);
+            }}>
+            <View style={{flex: 7, padding: 10}}>
+              <Heading2 style={[{textAlignVertical: 'center'}]}>
+                {item.title}
+              </Heading2>
+              <Heading5>
+                {item.totalVc} {t('vaccinesTxt')},{item.doneVc}{' '}
+                {t('vaccinesDoneTxt')} | {item.totalVc - item.doneVc}{' '}
+                {t('vaccinesPendingTxt')}
+              </Heading5>
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Icon
+                style={{alignSelf: 'center'}}
+                name={isOPen ? 'ic_angle_up' : 'ic_angle_down'}
+                size={10}
+                color="#000"
+              />
+            </View>
+          </Pressable>
+        </View>
+        {isOPen ? (
+          <>
+            <View>
+              <View style={{flexDirection: 'row'}}>
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      alignItems: 'center',
+                      lineHeight: 20,
+                      flexDirection: 'row',
+                      textAlign: 'center',
+                      width: 20,
+                      height: 20,
+                      borderRadius: 100,
+                      backgroundColor: 'green',
+                    }}>
+                    <Icon name="ic_tick" size={12} color="#FFF" />
+                  </Text>
+                </View>
+                <View style={{flex: 7, padding: 10}}>
+                  <Heading5>
+                    Diphtheria, tetanus, pertussis, polio, influenzae type b-
+                    the second dose
+                  </Heading5>
+                  <Pressable onPress={gotoArticle}>
+                    <Text style={{textDecorationLine: 'underline'}}>
+                      {t('vcArticleLink')}
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                borderBottomColor: props.subItemSaperatorColor,
+                borderBottomWidth: 1,
+                margin: 5,
+                padding: 1,
+              }}></View>
+            <View>
+              <View style={{flexDirection: 'row'}}>
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      alignItems: 'center',
+                      lineHeight: 20,
+                      flexDirection: 'row',
+                      textAlign: 'center',
+                      width: 20,
+                      height: 20,
+                      borderRadius: 100,
+                      backgroundColor: 'green',
+                    }}>
+                    <Icon name="ic_tick" size={12} color="#FFF" />
+                  </Text>
+                </View>
+                <View style={{flex: 7, padding: 10}}>
+                  <Heading5>
+                    Bacteria Streptococus pnuemoniae - the second dose
+                  </Heading5>
+                  <Pressable onPress={gotoArticle}>
+                    <Text style={{textDecorationLine: 'underline'}}>
+                      {t('vcArticleLink')}
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+            <View>
+              <View style={{alignItems: 'center', margin: 10}}>
+                <Pressable onPress={() =>
+                    navigation.navigate('AddChildVaccination', {
+                      headerTitle: t('editVcTitle'),
+                    })
+                  }>
+                  <Text style={{textDecorationLine: 'underline'}}>
+                    {t('vcEditDoctorData')}
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+          </>
+        ) : null}
+      </View>
+    </>
+  );
+};
+export default PreviousVaccines;
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    color: '#000',
+    backgroundColor: '#FFF',
+  },
+});
--- app/screens/home/Vaccination.tsx
+++ app/screens/home/Vaccination.tsx
@@ -1,12 +1,19 @@
 import FocusAwareStatusBar from '@components/FocusAwareStatusBar';
-import { ButtonPrimary, ButtonText } from '@components/shared/ButtonGlobal';
+import PreviousVaccines from '@components/PreviousVaccines';
 import TabScreenHeader from '@components/TabScreenHeader';
+import UpcomingVaccines from '@components/UpcomingVaccines';
 import { HomeDrawerNavigatorStackParamList } from '@navigation/types';
 import { StackNavigationProp } from '@react-navigation/stack';
+import {
+  Heading2,
+  Heading3,
+  Heading4Center,
+  Paragraph
+} from '@styles/typography';
 import React, { useContext } from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { Pressable, SafeAreaView, ScrollView, View } from 'react-native';
 import { ThemeContext } from 'styled-components';
-
 type VaccinationNavigationProp =
   StackNavigationProp<HomeDrawerNavigatorStackParamList>;
 type Props = {
@@ -16,6 +23,80 @@ const Vaccination = ({navigation}: Props) => {
   const themeContext = useContext(ThemeContext);
   const headerColor = themeContext.colors.VACCINATION_COLOR;
   const backgroundColor = themeContext.colors.VACCINATION_TINTCOLOR;
+  const {t} = useTranslation();
+  const vcitems = [
+    {
+      id: 0,
+      title: 'With 3-5 Months',
+      totalVc: 5,
+      doneVc: 0,
+    },
+    {
+      id: 1,
+      title: 'With Full 5 months',
+      totalVc: 6,
+      doneVc: 2,
+    },
+    {
+      id: 2,
+      title: 'With Full 6 months',
+      totalVc: 8,
+      doneVc: 1,
+    },
+    {
+      id: 3,
+      title: 'With full 9 months',
+      totalVc: 7,
+      doneVc: 3,
+    },
+  ];
+  const prevVcitems = [
+    {
+      id: 0,
+      title: 'At Birth',
+      totalVc: 5,
+      doneVc: 5,
+    },
+    {
+      id: 1,
+      title: 'With Full 1 months',
+      totalVc: 6,
+      doneVc: 2,
+    },
+    {
+      id: 2,
+      title: 'With Full 2 months',
+      totalVc: 8,
+      doneVc: 8,
+    },
+    {
+      id: 3,
+      title: 'With full 3 months',
+      totalVc: 7,
+      doneVc: 3,
+    },
+  ];
+  const [selectedIndex, setSelectedIndex] = React.useState<number>(0);
+  const data = [{title: t('vcTab1')}, {title: t('vcTab2')}];
+  const renderItem = (index: number) => {
+    if (index === 0) {
+      return (
+        <View>
+          {vcitems.map((item, itemindex) => {
+            return <UpcomingVaccines item={item} key={itemindex} headerColor={headerColor} backgroundColor={backgroundColor}/>;
+          })}
+        </View>
+      );
+    } else if (index === 1) {
+      return (
+        <View>
+          {prevVcitems.map((item, itemindex) => {
+            return <PreviousVaccines item={item} key={itemindex} headerColor={headerColor} backgroundColor={backgroundColor}/>;
+          })}
+        </View>
+      );
+    }
+  };
   return (
     <>
       <SafeAreaView style={{flex: 1}}>
@@ -25,14 +106,99 @@ const Vaccination = ({navigation}: Props) => {
             flexDirection: 'column',
             flex: 1,
           }}>
-          <TabScreenHeader title="Vaccination" headerColor={headerColor} textColor='#FFF'/>
-          <ScrollView style={{flex: 4, backgroundColor: backgroundColor}}>
-            <View>
+          <TabScreenHeader
+            title="Vaccination"
+            headerColor={headerColor}
+            textColor="#000"
+          />
+          <ScrollView style={{flex: 4}}>
+            <View style={{backgroundColor: backgroundColor}}>
+              <Heading3>{t('vcSummaryHeader')}</Heading3>
+              <View style={{padding: 5}}>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <View
+                    style={{
+                      padding: 10,
+                      margin: 5,
+                      backgroundColor: '#FFF',
+                      flex: 1,
+                      alignItems: 'center',
+                    }}>
+                    <Heading2>22</Heading2>
+                    <Paragraph>{t('vcStatus1')}</Paragraph>
+                  </View>
+                  <View
+                    style={{
+                      padding: 10,
+                      margin: 5,
+                      backgroundColor: '#FFF',
+                      flex: 1,
+                      alignItems: 'center',
+                    }}>
+                    <Heading2>4</Heading2>
+                    <Paragraph>{t('vcStatus2')}</Paragraph>
+                  </View>
+                  <View
+                    style={{
+                      padding: 10,
+                      margin: 5,
+                      backgroundColor: '#FFF',
+                      flex: 1,
+                      alignItems: 'center',
+                    }}>
+                    <Heading2>42</Heading2>
+                    <Paragraph>{t('vcStatus3')}</Paragraph>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                backgroundColor: '#FFF',
+                justifyContent: 'center',
+                padding: 5,
+              }}>
+              {data.map((item, itemindex) => {
+                return (
+                  <Pressable
+                    key={itemindex}
+                    style={{flex: 1}}
+                    onPress={() => {
+                      setSelectedIndex(itemindex);
+                    }}>
+                    <View
+                      style={[
+                        {
+                          backgroundColor:
+                            itemindex == selectedIndex
+                              ? headerColor
+                              : backgroundColor,
+                          padding: 10,
+                          margin: 3,
+                        },
+                      ]}>
+                      <Heading4Center>{item.title}</Heading4Center>
+                    </View>
+                  </Pressable>
+                );
+              })}
+            </View>
+
+            <View style={{flex: 1, width: '100%'}}>
+              {renderItem(selectedIndex)}
+            </View>
+            {/* <View>
             <ButtonPrimary
                     onPress={() => navigation.navigate('AddChildVaccination',{headerTitle:'Add Vaccination Data'})}>
                     <ButtonText>AddChildVaccination</ButtonText>
                   </ButtonPrimary>
-            </View>
+            </View> */}
           </ScrollView>
         </View>
       </SafeAreaView>
