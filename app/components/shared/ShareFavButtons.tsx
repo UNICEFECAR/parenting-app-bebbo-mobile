4a3e53ebdfca7fcfcb0ebd431781cc20e9031bbe
Mayur Baldha <mayur.baldha@datamatics.com>
2021-06-29 13:59:15 +0530

    Share function added
    Share function added

--- app/components/shared/ShareFavButtons.tsx
+++ app/components/shared/ShareFavButtons.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Pressable, Text, View } from 'react-native';
+import { Alert, Pressable, Share, Text, View } from 'react-native';
 import Icon from './Icon';
 
 const ShareFavButtons = (props:any) => {
   const {t} = useTranslation();
   const {backgroundColor,isFavourite}= props;
+  const onShare = async () => {
+    console.log("share");
+    try {
+      const result = await Share.share({
+        message:
+          'Parenbuddy App | An App for parents to monitor and guide your child growth',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error: any) {
+      Alert.alert(error.message);
+    }
+  };
   return (
     <>
     <View style={{flexDirection: 'row', padding: 10,backgroundColor:backgroundColor}}>
         <View style={{flex: 1}}>
-          <Pressable onPress={() => {}} style={{flexDirection: 'row'}}>
+          <Pressable onPress={() => {onShare()}} style={{flexDirection: 'row'}}>
             <Icon name="ic_sb_shareapp" size={20} color="#000" />
             <Text>{t('actScreenshareText')}</Text>
           </Pressable>
