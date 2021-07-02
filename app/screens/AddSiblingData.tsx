a4b5e9ca96fa89226b81261043fdd79919238cf4
Kalyani790 <kalyani.kaushik@datamatics.com>
2021-07-02 14:19:32 +0530

    isExpected true added,
    isExpected true added,
    luxon datetime show in fomat
    luxon support added for datetime picker restriction

--- app/screens/AddSiblingData.tsx
+++ app/screens/AddSiblingData.tsx
@@ -6,10 +6,11 @@ import Icon from '@components/shared/Icon';
 import OnboardingContainer from '@components/shared/OnboardingContainer';
 import OnboardingHeading from '@components/shared/OnboardingHeading';
 import { RootStackParamList } from '@navigation/types';
+import { useFocusEffect } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Pressable, View } from 'react-native';
+import { Alert, Pressable, View } from 'react-native';
 import { useAppDispatch } from '../../App';
 import { addChild, getNewChild } from '../services/childCRUD';
 import { Heading1Centerw, ShiftFromTop5 } from '../styles/typography';
@@ -37,14 +38,24 @@ const AddSiblingData = ({ route, navigation }: Props) => {
   const [birthDate, setBirthDate] = useState<Date>();
   const [plannedTermDate, setPlannedTermDate] = useState<Date>();
   const [isPremature, setIsPremature] = useState<string>('false');
+  const [isExpected,setIsExpected] = useState<string>('false');
   const sendData = (data: any) => { // the callback. Use a better name
+    console.log(data,"..data..")
     setBirthDate(data.birthDate);
     setPlannedTermDate(data.dueDate);
     var myString: string = String(data.isPremature);
     setIsPremature(myString);
+    setIsExpected(String(data.isExpected));
   };
+  useFocusEffect(
+    React.useCallback(() => {
+    if(childData!=null && childData.uuid!=''){
+      sendData(childData);
+    }
+    }, [])
+  );
   const AddChild=async ()=>{
-    let insertData: any = editScreen ? await getNewChild(uuid, plannedTermDate, isPremature,birthDate,relationship,name) : await getNewChild('', plannedTermDate, isPremature,birthDate)
+    let insertData: any = editScreen ? await getNewChild(uuid,isExpected, plannedTermDate, isPremature,birthDate,relationship,name) : await getNewChild('',isExpected, plannedTermDate, isPremature,birthDate)
     let childSet: Array<any> = [];
     childSet.push(insertData);
     addChild(editScreen, 1, childSet, dispatch, navigation);
@@ -78,8 +89,28 @@ const AddSiblingData = ({ route, navigation }: Props) => {
               //   index: 0,
               //   routes: [{name: 'HomeDrawerNavigator'}],
               // })
-              //console.log(uuid,"..uuid..");
-             AddChild();
+              console.log(birthDate,"..birthDate..");
+              console.log(isPremature,"..isPremature..");
+              console.log(plannedTermDate,"..plannedTermDate..");
+              console.log(isExpected,"..isExpected..");
+               
+              AddChild();
+              // if(birthDate==null || birthDate==undefined){
+              //   Alert.alert('Please enter birth date');
+              // }
+              // else{
+              //   if(isPremature){
+              //     if(plannedTermDate==null || plannedTermDate==undefined){
+              //       Alert.alert('Please enter due date');
+              //     }
+              //     else{
+                   
+              //     }
+              //   }
+              //   else{
+              //     AddChild();
+              //   }
+              // }
             
             }}>
             <ButtonText>{t('childSetupListsaveBtnText')}</ButtonText>
