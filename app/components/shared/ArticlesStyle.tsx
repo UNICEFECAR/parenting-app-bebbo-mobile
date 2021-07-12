ebdaae8ef597349527c2efd470df90dd6632f944
NehaRuparelgit <neha.ruparel@datamatics.com>
2021-07-12 15:07:21 +0530

    Adding flatlist
    Adding flatlist

--- app/components/shared/ArticlesStyle.tsx
+++ app/components/shared/ArticlesStyle.tsx
@@ -10,11 +10,9 @@ export const ArticleListContent = styled.View`
   padding:10px 10px;
 `;
 export const ArticleListContainer = styled.View`
-  margin:10px 15px;
   flex:1;
   background:${props => props.theme.colors.SECONDARY_TEXTCOLOR};
   border-radius:4px;
-  overflow:hidden;
   
 `;
 export const RelatedArticleContainer = styled(ArticleListContainer)`
