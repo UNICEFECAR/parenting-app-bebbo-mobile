9a2a66863a48b88458149814dfe3734cb5093d18
Mayur Baldha <mayur.baldha@datamatics.com>
2021-07-12 12:06:26 +0530

    ui  translation fixes

--- app/components/TabScreenHeader.tsx
+++ app/components/TabScreenHeader.tsx
@@ -1,16 +1,7 @@
-import { useFocusEffect } from '@react-navigation/native';
 import {
   Heading2w
 } from '@styles/typography';
 import React from 'react';
-import {
-  StyleSheet
-} from 'react-native';
-import { useAppDispatch } from '../../App';
-import {
-  getAllChildren,
-  getAllConfigData
-} from '../services/childCRUD';
 import HeaderBabyMenu from './HeaderBabyMenu';
 import BurgerIcon from './shared/BurgerIcon';
 import {
@@ -19,13 +10,6 @@ import {
 } from './shared/HeaderContainerStyle';
 const headerHeight = 50;
 const TabScreenHeader = (props: any) => {
-  const dispatch = useAppDispatch();
-  // useFocusEffect(
-  //   React.useCallback(() => {
-  //     getAllChildren(dispatch);
-  //     getAllConfigData(dispatch);
-  //   }, []),
-  // );
   const headerColor = props.headerColor;
   const textColor = props.textColor;
  
@@ -42,45 +26,9 @@ const TabScreenHeader = (props: any) => {
           <Heading2w style={{color:textColor}}> {props.title}</Heading2w>
         </HeaderTitleView>
         <HeaderBabyMenu color={textColor}/>
-        {/* <HeaderActionView>
-          <HeaderActionBox
-            onPress={() => {
-              // console.log(modalVisible);
-              if (modalVisible) {
-                setModalVisible(false);
-              } else {
-                setModalVisible(true);
-              }
-            }}>
-            <Icon name="ic_baby" size={25} color="#000" />
-          </HeaderActionBox>
-        </HeaderActionView> */}
       </HeaderRowView>
     </>
   );
 };
 export default TabScreenHeader;
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    // alignItems: 'center',
-    paddingTop: headerHeight,
-  },
 
-  modalView: {
-    // margin: 20,
-    backgroundColor: 'white',
-    borderBottomLeftRadius: 4,
-    borderBottomRightRadius: 4,
-    padding: 0,
-
-    borderColor: '#000',
-    borderBottomWidth: 2,
-  },
-
-  modalText: {
-    textAlign: 'center',
-    borderBottomWidth: 2,
-  },
-});
