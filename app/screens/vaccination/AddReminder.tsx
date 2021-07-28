0823fee85c5466846c1cde636679d8d0101d15f0
Mayur Baldha <mayur.baldha@datamatics.com>
2021-07-28 15:26:49 +0530

    reminders module implementation  for vaccine and health checkup
    reminders module implementation  for vaccine and health checkup

--- app/screens/vaccination/AddReminder.tsx
+++ app/screens/vaccination/AddReminder.tsx
@@ -1,12 +1,24 @@
 import FocusAwareStatusBar from '@components/FocusAwareStatusBar';
-import { ButtonColTwo, ButtonContainerTwo, ButtonSecondary, ButtonSecondaryTint, ButtonTertiary, ButtonText } from '@components/shared/ButtonGlobal';
+import {
+  ButtonColTwo,
+  ButtonContainerTwo,
+  ButtonSecondary,
+  ButtonSecondaryTint,
+  ButtonTertiary,
+  ButtonText
+} from '@components/shared/ButtonGlobal';
 import {
   FormDateAction,
   FormDateText,
   FormInputBox,
   FormInputGroup
 } from '@components/shared/ChildSetupStyle';
-import { HeaderActionView, HeaderIconView, HeaderRowView, HeaderTitleView } from '@components/shared/HeaderContainerStyle';
+import {
+  HeaderActionView,
+  HeaderIconView,
+  HeaderRowView,
+  HeaderTitleView
+} from '@components/shared/HeaderContainerStyle';
 import Icon from '@components/shared/Icon';
 import ModalPopupContainer, {
   PopupClose,
@@ -17,17 +29,23 @@ import { RootStackParamList } from '@navigation/types';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { StackNavigationProp } from '@react-navigation/stack';
 import {
-  Heading2, Heading3Center,
-  Paragraph, ShiftFromTop20, ShiftFromTop30, ShiftFromTopBottom10
+  Heading2,
+  Heading3Center,
+  Paragraph,
+  ShiftFromTop20,
+  ShiftFromTop30,
+  ShiftFromTopBottom10
 } from '@styles/typography';
+import { DateTime } from 'luxon';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import {
-  Modal, Pressable,
-  SafeAreaView,
-  Text, View
-} from 'react-native';
+import { Modal, Pressable, SafeAreaView, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { v4 as uuidv4 } from 'uuid';
+import { useAppDispatch, useAppSelector } from '../../../App';
+import { userRealmCommon } from '../../database/dbquery/userRealmCommon';
+import { ChildEntity, ChildEntitySchema } from '../../database/schema/ChildDataSchema';
+import { setActiveChild } from '../../services/childCRUD';
 type ChildSetupNavigationProp = StackNavigationProp<RootStackParamList>;
 
 type Props = {
@@ -36,181 +54,236 @@ type Props = {
 //  reminder.periodId === doctorVisitPeriod.uuid
 const AddReminder = ({route, navigation}: any) => {
   const {t} = useTranslation();
-  const {headerTitle,buttonTitle,titleTxt,warningTxt,headerColor,reminderType} = route.params;
-  const [measureDate, setmeasureDate] = useState<Date>();
+  const {
+    headerTitle,
+    buttonTitle,
+    titleTxt,
+    warningTxt,
+    headerColor,
+    reminderType,
+    editReminderItem,
+  } = route.params;
+  // console.log(editReminderItem,"editReminderItem");
+  const [measureDate, setmeasureDate] = useState<DateTime>( editReminderItem ? editReminderItem.reminderDate : null,);
   const [showmeasure, setmeasureShow] = useState<Boolean>(false);
   const [modalVisible, setModalVisible] = useState(false);
-  const [measureTime, setmeasureTime] = useState<any>();
+  const [measureTime, setmeasureTime] = useState<DateTime>(editReminderItem ? editReminderItem.reminderTime : null,);
   const [showmeasureTime, setmeasureShowTime] = useState<Boolean>(false);
-const defaultDatePickerValue =new Date(2021, 10, 20);
-const defaulttimePickerValue =new Date();
-
-
+  const [dateTouched, setDateTouched] = useState<Boolean>(false);
+  const [timeTouched, setTimeTouched] = useState<Boolean>(false);
+  // const defaultDatePickerValue = new Date();
+  // const defaulttimePickerValue = new Date();
+  const languageCode = useAppSelector(
+    (state: any) => state.selectedCountry.languageCode,
+  );
+  const dispatch = useAppDispatch();
+  const activeChild = useAppSelector((state: any) =>
+    state.childData.childDataSet.activeChild != ''
+      ? JSON.parse(state.childData.childDataSet.activeChild)
+      : [],
+  );
+  const child_age = useAppSelector((state: any) =>
+  state.utilsData.taxonomy.allTaxonomyData != ''
+    ? JSON.parse(state.utilsData.taxonomy.allTaxonomyData).child_age
+    : [],
+);
   const onmeasureChange = (event: any, selectedDate: any) => {
     const currentDate = selectedDate || measureDate;
     setmeasureShow(false);
-    setmeasureDate(currentDate);
+    if (selectedDate) {
+    setmeasureDate(DateTime.fromJSDate(currentDate));
+    setDateTouched(true);
+    }
   };
   const showmeasureDatepicker = () => {
     setmeasureShow(true);
   };
 
   const onmeasureTimeChange = (event: any, selectedTime: any) => {
-   // console.log(selectedTime);
-    const currentTime = selectedTime.getHours()+":"+selectedTime.getMinutes() || measureTime;
+    // console.log(selectedTime,"selectedTime");
+    const currentTime = selectedTime || measureTime;
+    // console.log(currentTime,"currentTime");
     setmeasureShowTime(false);
-    setmeasureTime(currentTime);
+    setmeasureTime(DateTime.fromJSDate(currentTime));
+    setTimeTouched(true)
+    // console.log(reminderType, measureDate, measureTime);
   };
   const showmeasureTimepicker = () => {
     setmeasureShowTime(true);
+  };
+  const deleteReminder =  async () => {
+    
+    let createresult = await userRealmCommon.deleteChildReminders<ChildEntity>(
+      ChildEntitySchema,
+      editReminderItem.uuid,
+      'uuid ="' + activeChild.uuid + '"',
+    );
+    // console.log(createresult,"ReminderDeleted");
+    setActiveChild(languageCode, activeChild.uuid, dispatch, child_age);
+   
+  }
+  const saveReminder = async() => {
+    console.log(reminderType, measureDate, measureTime);
+    const reminderValues = {
+      uuid: editReminderItem ? editReminderItem.uuid : uuidv4(),
+      reminderType: reminderType,
+      reminderDate: editReminderItem ? ((dateTouched) ? measureDate?.toMillis() : measureDate):measureDate?.toMillis(),
+      reminderTime: editReminderItem ? ((timeTouched) ? measureTime?.toMillis() : measureTime):measureTime?.toMillis(),
+    };
+    let createresult = await userRealmCommon.updateChildReminders<ChildEntity>(
+      ChildEntitySchema,
+      reminderValues,
+      'uuid ="' + activeChild.uuid + '"',
+    );
+    // console.log(createresult);
+    setActiveChild(languageCode, activeChild.uuid, dispatch, child_age);
+    
   };
   return (
     <>
       <SafeAreaView style={{flex: 1, backgroundColor: headerColor}}>
         <FocusAwareStatusBar animated={true} backgroundColor={headerColor} />
         <HeaderRowView
-        style={{
-          backgroundColor: headerColor,
-          maxHeight: 50,
-        }}>
-          
-              <HeaderIconView>
-              <Pressable
-                onPress={() => {
-                  navigation.goBack();
-                }}>
-                <Icon name={'ic_back'} color="#000" size={15} />
-              </Pressable>
-              </HeaderIconView>
-              <HeaderTitleView>
-              <Heading2>{headerTitle}</Heading2>
-              </HeaderTitleView>
-              <HeaderActionView>
-              <Pressable
-              onPress={() => {
-                setModalVisible(true)
-              }}>
-                <Text>{t('growthScreendeletebtnText')}</Text>
-              </Pressable>
-              </HeaderActionView>
-              </HeaderRowView>
-        
-        <ScrollView style={{padding:15,flex:7}}>
-          <Paragraph>{titleTxt}</Paragraph>
-        <FormInputGroup onPress={showmeasureDatepicker}>
-          <FormInputBox>
-            <FormDateText>
-              <Text>
-                {measureDate
-                  ? measureDate.toDateString()
-                  : t('vcReminderDate')}
-              </Text>
-            </FormDateText>
-            <FormDateAction>
-              <Icon name="ic_calendar" size={20} color="#000" />
-            </FormDateAction>
-          </FormInputBox>
-        </FormInputGroup>
-<ShiftFromTop20>
-        <FormInputGroup onPress={showmeasureTimepicker}>
-          <FormInputBox>
-            <FormDateText>
-              <Text>
-                {measureTime
-                  ? (measureTime)
-                  : t('vcReminderTime')}
-              </Text>
-            </FormDateText>
-            <FormDateAction>
-              <Icon name="ic_time" size={20} color="#000" style={{borderWidth: 1, borderRadius: 50}} />
-            </FormDateAction>
-          </FormInputBox>
-        </FormInputGroup>
-        </ShiftFromTop20>
-        <View>
-          {showmeasure && (
-            <DateTimePicker
-              testID="measuredatePicker"
-              value={defaultDatePickerValue}
-              mode={'date'}
-              display="default"
-              minimumDate={new Date()}
-              // maximumDate => childDOB +72 weeks
-              onChange={onmeasureChange}
-            />
-          )}
-        </View>
-
-        <View>
-          {showmeasureTime && (
-            <DateTimePicker
-              testID="measuretimePicker"
-              value={defaulttimePickerValue}
-              mode={'time'}
-              display="default"
-              is24Hour={true}
-              minimumDate={new Date()}
-              onChange={onmeasureTimeChange}
-            />
-          )}
-        </View>
-        
-       
-        <ShiftFromTop30>
-          <ButtonTertiary
-          
-            onPress={() => {
-              navigation.goBack();
-            }}>
-            <ButtonText>{buttonTitle}</ButtonText>
-          </ButtonTertiary>
-          </ShiftFromTop30>
-        <Modal
-          animationType="none"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            // Alert.alert('Modal has been closed.');
-            setModalVisible(!modalVisible);
-          }}
-          onDismiss={() => {
-            setModalVisible(!modalVisible);
+          style={{
+            backgroundColor: headerColor,
+            maxHeight: 50,
           }}>
-          <PopupOverlay>
-            <ModalPopupContainer>
-              <PopupCloseContainer>
-                <PopupClose
-                  onPress={() => {
-                    setModalVisible(!modalVisible);
-                  }}>
-                  <Icon name="ic_close" size={16} color="#000" />
-                </PopupClose>
-              </PopupCloseContainer>
-              <ShiftFromTopBottom10>
-              <Heading3Center>
-                {warningTxt}
-              </Heading3Center>
-              </ShiftFromTopBottom10>
-              <ButtonContainerTwo>
-                <ButtonColTwo>
-                  <ButtonSecondaryTint>
-                  <ButtonText>{t('growthDeleteOption1')}</ButtonText>
-                  </ButtonSecondaryTint>
+          <HeaderIconView>
+            <Pressable
+              onPress={() => {
+                navigation.goBack();
+              }}>
+              <Icon name={'ic_back'} color="#000" size={15} />
+            </Pressable>
+          </HeaderIconView>
+          <HeaderTitleView>
+            <Heading2>{headerTitle}</Heading2>
+          </HeaderTitleView>
+         {editReminderItem ? <HeaderActionView>
+            <Pressable
+              onPress={() => {
+                setModalVisible(true);
+              }}>
+              <Text>{t('growthScreendeletebtnText')}</Text>
+            </Pressable>
+          </HeaderActionView> : null}
+        </HeaderRowView>
+
+        <ScrollView style={{padding: 15, flex: 7}}>
+          <Paragraph>{titleTxt}</Paragraph>
+          <FormInputGroup onPress={showmeasureDatepicker}>
+            <FormInputBox>
+              <FormDateText>
+                <Text>
+                  {measureDate
+                    ? DateTime.fromJSDate(new Date(measureDate)).toFormat(
+                      'dd/MM/yyyy',
+                    )
+                    : t('vcReminderDate')}
+                </Text>
+              </FormDateText>
+              <FormDateAction>
+                <Icon name="ic_calendar" size={20} color="#000" />
+              </FormDateAction>
+            </FormInputBox>
+          </FormInputGroup>
+          <ShiftFromTop20>
+            <FormInputGroup onPress={showmeasureTimepicker}>
+              <FormInputBox>
+                <FormDateText>
+                  <Text>{measureTime ? DateTime.fromJSDate(new Date(measureTime)).toFormat(
+                      'hh:mm') : t('vcReminderTime')}</Text>
+                </FormDateText>
+                <FormDateAction>
+                  <Icon
+                    name="ic_time"
+                    size={20}
+                    color="#000"
+                    style={{borderWidth: 1, borderRadius: 50}}
+                  />
+                </FormDateAction>
+              </FormInputBox>
+            </FormInputGroup>
+          </ShiftFromTop20>
+          <View>
+            {showmeasure && (
+              <DateTimePicker
+                testID="measuredatePicker"
+                value={editReminderItem ? new Date(measureDate) : new Date()}
+                mode={'date'}
+                display="default"
+                minimumDate={new Date()}
+                // maximumDate => childDOB +72 weeks
+                onChange={onmeasureChange}
+              />
+            )}
+          </View>
+
+          <View>
+            {showmeasureTime && (
+              <DateTimePicker
+                testID="measuretimePicker"
+                value={editReminderItem ? new Date(measureTime) : new Date()}
+                mode={'time'}
+                display="default"
+                is24Hour={true}
+                minimumDate={new Date()}
+                onChange={onmeasureTimeChange}
+              />
+            )}
+          </View>
+
+          <ShiftFromTop30>
+            <ButtonTertiary
+              disabled={!measureDate && !measureTime}
+              onPress={() => {
+                saveReminder().then(()=>{ navigation.goBack();});
+                // navigation.goBack();
+              }}>
+              <ButtonText>{buttonTitle}</ButtonText>
+            </ButtonTertiary>
+          </ShiftFromTop30>
+          <Modal
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              // Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+            }}
+            onDismiss={() => {
+              setModalVisible(!modalVisible);
+            }}>
+            <PopupOverlay>
+              <ModalPopupContainer>
+                <PopupCloseContainer>
+                  <PopupClose
+                    onPress={() => {
+                      setModalVisible(!modalVisible);
+                    }}>
+                    <Icon name="ic_close" size={16} color="#000" />
+                  </PopupClose>
+                </PopupCloseContainer>
+                <ShiftFromTopBottom10>
+                  <Heading3Center>{warningTxt}</Heading3Center>
+                </ShiftFromTopBottom10>
+                <ButtonContainerTwo>
+                  <ButtonColTwo>
+                    <ButtonSecondaryTint onPress={()=> setModalVisible(!modalVisible)}>
+                      <ButtonText>{t('growthDeleteOption1')}</ButtonText>
+                    </ButtonSecondaryTint>
                   </ButtonColTwo>
 
                   <ButtonColTwo>
-                  <ButtonSecondary>
-                  <ButtonText>{t('growthDeleteOption2')}</ButtonText>
+                    <ButtonSecondary onPress={()=> {setModalVisible(!modalVisible);deleteReminder().then(()=>{ navigation.goBack();});}}>
+                      <ButtonText>{t('growthDeleteOption2')}</ButtonText>
                     </ButtonSecondary>
-                    </ButtonColTwo>
-                
-              </ButtonContainerTwo>
-              
-            </ModalPopupContainer>
-          </PopupOverlay>
-        </Modal>
+                  </ButtonColTwo>
+                </ButtonContainerTwo>
+              </ModalPopupContainer>
+            </PopupOverlay>
+          </Modal>
         </ScrollView>
-        
       </SafeAreaView>
     </>
   );
