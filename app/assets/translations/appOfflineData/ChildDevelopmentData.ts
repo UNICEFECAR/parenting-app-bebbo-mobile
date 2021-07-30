cfa88b809ef7c9318aabad2297d79cfe0df5d4d0
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-07-30 14:03:17 +0530

    child profile cursor issue solved
    1.child profile cursor issue solved.
    2.gender position changes on setup.
    3.label added for gender.
     4. en language data change.

--- app/assets/translations/appOfflineData/ChildDevelopmentData.ts
+++ app/assets/translations/appOfflineData/ChildDevelopmentData.ts
@@ -58,5 +58,35 @@ export const ChildDevelopmentData:any = {
             "updated_at": "Thu, 07/15/2021 - 13:55",
             "mandatory": 1
         }
-    ]
+    ],
+    "rs-en": [
+        {
+            "id": 12001,
+            "type": "Child Development",
+            "title": "1 month- Getting to know each other",
+            "child_age": [
+                43
+            ],
+            "boy_video_article": 981,
+            "girl_video_article": 986,
+            "milestone": "<p>Watch your baby's behaviour and talk to your paediatrician or visiting nurse if you notice that at the end of the first month your baby: </p>\n\n<ul><li>is crying a lot, and you are not able to comfort your baby</li>\n\t<li>sucks poorly and feeds very slowly</li>\n\t<li>sleeps more than expected (more than 16 hours a day), or does not sleep peacefully</li>\n\t<li>is not moving her/his arms or legs at all</li>\n\t<li>does not startle when a loud noise is made</li>\n\t<li>does not blink at bright lights.</li>\n</ul>",
+            "created_at": "Tue, 07/13/2021 - 18:56",
+            "updated_at": "Thu, 07/15/2021 - 15:22",
+            "mandatory": 1
+        },
+        {
+            "id": 12002,
+            "type": "Child Development",
+            "title": "2 months- Mutual adjustments  and establishing routines",
+            "child_age": [
+                44
+            ],
+            "boy_video_article": 0,
+            "girl_video_article": 0,
+            "milestone": "<p>\nWatch your baby's behaviour and talk to your paediatrician or visiting nurse if you notice that at the end of the second month your baby:\n</p>\n<ul>\n<li>\nfrequently cries a lot, and you are not usually able to soothe you baby\n</li>\n<li> <br />\nisn’t watching faces even for a short time\n</li>\n<li>   <br />\nisn’t responding to bright light or loud sounds\n</li>\n<li> <br />\nisn’t feeding or sleeping well (or sleeps more than 16 hours a day)\n</li>\n<li>  <br />\nisn’t moving her/his arms or legs at all\n</li>\n<li> <br />\nisn`t bringing her/his hands to her/his mouth \n</li>\n<li>  <br />\nisn’t smiling at people.\n</li>\n</ul>\n",
+            "created_at": "Tue, 07/13/2021 - 18:56",
+            "updated_at": "Thu, 07/15/2021 - 13:55",
+            "mandatory": 1
+        }
+    ],
 };
