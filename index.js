ff36619808e733162c3fd8005efdef95da470318
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-08-05 19:56:17 +0530

    updated index.js
    updated index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ import React from 'react';
 import { Text}  from'react-native';
 // import 'intl';
 // import 'intl/locale-data/jsonp/en';
+AppRegistry.registerComponent(appName, () => () => <App />);
 if (Text.defaultProps == null) {
     Text.defaultProps = {};
     Text.defaultProps.allowFontScaling = false;
 }
-AppRegistry.registerComponent(appName, () => () => <App />);
