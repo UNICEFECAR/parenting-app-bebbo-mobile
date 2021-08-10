bd0a2fbbf3211cfd73eb2e57c00319d170ce2d66
BhumikaDatamatics <bhumika.pathak@datamatics.com>
2021-08-10 18:45:45 +0530

    Update decrypt_secret.sh

--- android/decrypt_secret.sh
+++ android/decrypt_secret.sh
@@ -5,5 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
-
 --output $HOME/secrets/pbdebug.keystore pbdebug.keystore.gpg
