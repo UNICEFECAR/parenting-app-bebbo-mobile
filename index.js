eee827041d97e494cf9291ab6ea113fe4ed769a3
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-08-19 23:53:24 +0530

    localize code setup
    localize code setup

--- index.js
+++ index.js
@@ -2,15 +2,36 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import React from 'react';
-import { Text}  from'react-native';
-// import 'intl';
-// import 'intl/locale-data/jsonp/en';
-AppRegistry.registerComponent(appName, () => () => <App />);
-if (Text.defaultProps == null) {
-    Text.defaultProps = {};
-    Text.defaultProps.allowFontScaling = false;
-}
+ import {AppRegistry} from 'react-native';
+ import App from './App';
+ import {name as appName} from './app.json';
+ import React from 'react';
+ import { Text}  from'react-native';
+ // import 'intl';
+ // import 'intl/locale-data/jsonp/en';
+ import 'intl';
+ 
+ import 'intl/locale-data/jsonp/sq-AL';
+ import 'intl/locale-data/jsonp/be-BY';
+ import 'intl/locale-data/jsonp/ru-BY';
+ import 'intl/locale-data/jsonp/bg-BG';
+ import 'intl/locale-data/jsonp/el-GR';
+ import 'intl/locale-data/jsonp/sq-XK';
+ import 'intl/locale-data/jsonp/sr-Latn-XK';
+ import 'intl/locale-data/jsonp/ky-KG';
+ import 'intl/locale-data/jsonp/ru-KG';
+ import 'intl/locale-data/jsonp/sr-Cyrl-ME';//montenegrin code to be verified
+ import 'intl/locale-data/jsonp/mk-MK';
+ import 'intl/locale-data/jsonp/sq-MK';
+ import 'intl/locale-data/jsonp/sr-Latn-RS';
+ import 'intl/locale-data/jsonp/uz-Latn-UZ';
+ import 'intl/locale-data/jsonp/ru-RU';
+ // import 'intl/locale-data/jsonp/be';
+ // import 'intl/locale-data/jsonp/be';
+ //sq,ru
+ AppRegistry.registerComponent(appName, () => () => <App />);
+ if (Text.defaultProps == null) {
+     Text.defaultProps = {};
+     Text.defaultProps.allowFontScaling = false;
+ }
+ 
