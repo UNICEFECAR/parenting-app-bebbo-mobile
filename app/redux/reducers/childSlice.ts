6b17df57e9da0c01c443aa4fba9ce20cde5a4b9c
NehaRuparelgit <neha.ruparel@datamatics.com>
2021-08-26 20:31:56 +0530

    Language change condition
    - Promise added while deleting on language change.
    - Age bracket emptied on language change.

--- app/redux/reducers/childSlice.ts
+++ app/redux/reducers/childSlice.ts
@@ -60,17 +60,22 @@ export const childSlice = createSlice({
       state,
       action: PayloadAction<any>,
     ) => {
-     console.log("bufferAgeBracket",action.payload);
+     console.log("bufferAgeBracket",action.payload.length);
     console.log("state.bufferAgeBracket---",state.childDataSet.bufferAgeBracket);
       // state.bufferAgeBracket = action.payload;
       // if(state.childDataSet.bufferAgeBracket)
       // {
-        action.payload.map((value:any)=>{
-          const i = state.childDataSet.bufferAgeBracket.findIndex(_item => _item === value);
-          if(i == -1){
-            state.childDataSet.bufferAgeBracket.push(value) 
-          }
-        });
+        if(action.payload.length > 0)
+        {
+          action.payload.map((value:any)=>{
+            const i = state.childDataSet.bufferAgeBracket.findIndex(_item => _item === value);
+            if(i == -1){
+              state.childDataSet.bufferAgeBracket.push(value) 
+            }
+          });
+        }else {
+          state.childDataSet.bufferAgeBracket = [];
+        }
       // }else {
       //   state.childDataSet.bufferAgeBracket = action.payload;
       // }
