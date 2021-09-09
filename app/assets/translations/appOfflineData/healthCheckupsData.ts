908222eabbeb40c05c86309e070ae178ef8b1919
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-09-09 19:53:17 +0530

    offline file
    offline file

--- app/assets/translations/appOfflineData/healthCheckupsData.ts
+++ app/assets/translations/appOfflineData/healthCheckupsData.ts
@@ -1,217 +1,114 @@
 export const healthCheckupsData:any = {
-     //api all data
-    "en":[
+    "en": [
         {
             "id": 56251,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 6 years ",
             "growth_period": 6553,
             "pinned_article": 6896,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 12:00",
-            "updated_at": "Mon, 08/02/2021 - 12:48"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56246,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 3 years ",
             "growth_period": 6546,
             "pinned_article": 4511,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:59",
-            "updated_at": "Wed, 08/04/2021 - 12:15"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56241,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 18 months",
             "growth_period": 6560,
             "pinned_article": 4476,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:58",
-            "updated_at": "Mon, 08/02/2021 - 12:51"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56236,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "From 12 to 15 months",
             "growth_period": 6533,
             "pinned_article": 4471,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:57",
-            "updated_at": "Mon, 08/02/2021 - 12:51"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56231,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 9 months",
             "growth_period": 6521,
             "pinned_article": 4466,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:57",
-            "updated_at": "Mon, 08/02/2021 - 12:52"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56226,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 6 months",
             "growth_period": 6501,
             "pinned_article": 4461,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:56",
-            "updated_at": "Mon, 08/02/2021 - 12:56"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56221,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 5 months",
             "growth_period": 6496,
             "pinned_article": 4456,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:55",
-            "updated_at": "Mon, 08/02/2021 - 12:54"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56216,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 3.5 months",
             "growth_period": 6486,
             "pinned_article": 4451,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:54",
-            "updated_at": "Mon, 08/02/2021 - 12:55"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56211,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 2 months",
             "growth_period": 6476,
             "pinned_article": 4446,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:53",
-            "updated_at": "Tue, 07/20/2021 - 11:54"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 56206,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "With full 1 month",
             "growth_period": 6471,
             "pinned_article": 4441,
             "pinned_video_article": 0,
             "created_at": "Tue, 07/20/2021 - 11:51",
-            "updated_at": "Tue, 07/20/2021 - 11:53"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         },
         {
             "id": 23081,
-            "type": "Health Check-ups",
+            "type": "Health Check-ups - Age Periods",
             "title": "At birth",
             "growth_period": 6466,
             "pinned_article": 4526,
             "pinned_video_article": 0,
             "created_at": "Thu, 07/15/2021 - 06:39",
-            "updated_at": "Tue, 07/20/2021 - 11:51"
-        }
-    ],
-    "rs-sr":  [
-        {
-            "id": 23766,
-            "type": "Health Check-ups",
-            "title": "između 6 i 7 godina",
-            "growth_period": 59056,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:24",
-            "updated_at": "Mon, 08/02/2021 - 13:08"
-        },
-        {
-            "id": 23746,
-            "type": "Health Check-ups",
-            "title": "između 18 i 24 meseca",
-            "growth_period": 59061,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:22",
-            "updated_at": "Mon, 08/02/2021 - 13:16"
-        },
-        {
-            "id": 23726,
-            "type": "Health Check-ups",
-            "title": "između 12 i 15 meseci",
-            "growth_period": 6533,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:20",
-            "updated_at": "Mon, 08/02/2021 - 13:17"
-        },
-        {
-            "id": 23716,
-            "type": "Health Check-ups",
-            "title": "sa punih 9 meseci",
-            "growth_period": 6521,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:19",
-            "updated_at": "Mon, 08/02/2021 - 13:18"
-        },
-        {
-            "id": 23696,
-            "type": "Health Check-ups",
-            "title": "sa punih 6 meseci",
-            "growth_period": 6501,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:18",
-            "updated_at": "Mon, 08/02/2021 - 13:18"
-        },
-        {
-            "id": 23671,
-            "type": "Health Check-ups",
-            "title": "sa punih 5 meseci",
-            "growth_period": 6496,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:16",
-            "updated_at": "Mon, 08/02/2021 - 13:18"
-        },
-        {
-            "id": 23661,
-            "type": "Health Check-ups",
-            "title": "sa 3.5 meseca",
-            "growth_period": 6486,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:14",
-            "updated_at": "Mon, 08/02/2021 - 13:19"
-        },
-        {
-            "id": 23646,
-            "type": "Health Check-ups",
-            "title": "sa puna 2 meseca",
-            "growth_period": 6476,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:13",
-            "updated_at": "Sat, 07/17/2021 - 16:09"
-        },
-        {
-            "id": 23636,
-            "type": "Health Check-ups",
-            "title": "sa punih mesec dana",
-            "growth_period": 6471,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:13",
-            "updated_at": "Sat, 07/17/2021 - 16:09"
-        },
-        {
-            "id": 23611,
-            "type": "Health Check-ups",
-            "title": "na rođenju",
-            "growth_period": 6466,
-            "pinned_article": 0,
-            "pinned_video_article": 0,
-            "created_at": "Fri, 07/16/2021 - 14:00",
-            "updated_at": "Sat, 07/17/2021 - 16:09"
+            "updated_at": "Mon, 08/16/2021 - 14:15"
         }
     ]
 };
