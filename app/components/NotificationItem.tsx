74a769b4f114810f532ec0b77adaa4a37ff14ff2
Mayur Baldha <82522186+mayurbaldha91@users.noreply.github.com>
2021-09-15 19:48:43 +0530

    filter of notifications with reminders notifications added
    hc, vc reminder and count added

--- app/components/NotificationItem.tsx
+++ app/components/NotificationItem.tsx
@@ -13,7 +13,9 @@ import {
   renderers
 } from 'react-native-popup-menu';
 import { ThemeContext } from 'styled-components/native';
-import { getCurrentChildAgeInDays, getCurrentChildAgeInMonths } from '../services/childCRUD';
+import { useAppSelector } from '../../App';
+import { getCurrentChildAgeInMonths } from '../services/childCRUD';
+import { formatStringDate, formatStringTime } from '../services/Utils';
 import { ButtonTextSmLineL } from './shared/ButtonGlobal';
 import Checkbox, { CheckboxActive, CheckboxItem } from './shared/CheckboxStyle';
 import { FormOuterCheckbox } from './shared/ChildSetupStyle';
@@ -23,7 +25,7 @@ import { NotifAction, NotificationListContainer, NotifIcon, NotifiContent } from
 
 
 const NotificationItem = (props: any) => {
-  const { item, itemIndex, onItemReadMarked, onItemDeleteMarked, selectedCategories, isDeleteEnabled, childAgeInDays, activeChild } = props;
+  const { item, itemIndex, onItemReadMarked, onItemDeleteMarked, isDeleteEnabled, childAgeInDays, activeChild } = props;
   const themeContext = useContext(ThemeContext);
   const hcheaderColor = themeContext.colors.HEALTHCHECKUP_COLOR;
   const navigation = useNavigation();
@@ -35,9 +37,9 @@ const NotificationItem = (props: any) => {
       ? 'ic_growth'
       : type == 'cd'
         ? 'ic_milestone'
-        : type == 'vc'
+        : type == 'vc' || type == 'vcr'
           ? 'ic_vaccination'
-          : type == 'hc'
+          : type == 'hc' || type == 'hcr'
             ? 'ic_doctor_chk_up'
             : 'ic_growth';
   };
@@ -46,11 +48,11 @@ const NotificationItem = (props: any) => {
       ? t('growthScreenaddNewBtntxt')
       : type == 'cd'
         ? t('trackMilestoneViewBtn')
-        : type == 'vc'
+        : type == 'vc' || type == 'vcr'
           ? t('vcAddBtn')
           : type == 'hc'
             ? t('hcReminderbtn')
-            : '';
+            : type == 'hcr' ? t('hcNewBtn') : '';
   };
   const gotoPage = (type: string) => {
     //console.log(type);
@@ -60,7 +62,7 @@ const NotificationItem = (props: any) => {
       })
       : type == 'cd'
         ? navigation.navigate('Home', { screen: 'ChildDevelopment' })
-        : type == 'vc'
+        : type == 'vc' || type == 'vcr'
           ? navigation.navigate('Home', {
             screen: 'Tools',
             params: {
@@ -76,7 +78,13 @@ const NotificationItem = (props: any) => {
               warningTxt: t('hcReminderDeleteWarning'),
               headerColor: hcheaderColor,
             })
-            : '';
+            : type == 'hcr'
+              ? navigation.navigate('Home', {
+                screen: 'Tools',
+                params: {
+                  screen: 'HealthCheckupsTab',
+                },
+              }) : '';
   };
   const markAsRead = (item: any) => {
     onItemReadMarked(item);
@@ -93,31 +101,31 @@ const NotificationItem = (props: any) => {
   useEffect(() => {
     setToggleCheckBox(false);
   }, [isDeleteEnabled])
-  const IsGrowthMeasuresForPeriodExist = () => {
-    // isGrowthMeasureExistForDate(selectedMeasureDate,activeChild)
-    // if item.days_to is today's date and thne check measures not entered then only show
-    let isGrowthNotMeasureExist = true;
-    if (activeChild.measures.length > 0) {
-      activeChild.measures.forEach((measure) => {
-        const childMeasureDateInDays = getCurrentChildAgeInDays(
-          DateTime.fromJSDate(new Date(measure.measurementDate)).toMillis(),
-        );
-        if (item.days_from < childMeasureDateInDays && item.days_to > childMeasureDateInDays) {
-          isGrowthNotMeasureExist = false;
-        } else {
-          isGrowthNotMeasureExist = true;
-        }
-        //get measurementdate in days and check if it is in between
-        //find if measure exists in day_from and day_to
-      })
-    }
-    // console.log(isGrowthNotMeasureExist, 'isGrowthMeasureExist')
-    return isGrowthNotMeasureExist
-  }
+  // const IsGrowthMeasuresForPeriodExist = () => {
+  //   // isGrowthMeasureExistForDate(selectedMeasureDate,activeChild)
+  //   // if item.days_to is today's date and thne check measures not entered then only show
+  //   let isGrowthNotMeasureExist = true;
+  //   if (activeChild.measures.length > 0) {
+  //     activeChild.measures.forEach((measure) => {
+  //       const childMeasureDateInDays = getCurrentChildAgeInDays(
+  //         DateTime.fromJSDate(new Date(measure.measurementDate)).toMillis(),
+  //       );
+  //       if (item.days_from < childMeasureDateInDays && item.days_to > childMeasureDateInDays) {
+  //         isGrowthNotMeasureExist = false;
+  //       } else {
+  //         isGrowthNotMeasureExist = true;
+  //       }
+  //       //get measurementdate in days and check if it is in between
+  //       //find if measure exists in day_from and day_to
+  //     })
+  //   }
+  //   // console.log(isGrowthNotMeasureExist, 'isGrowthMeasureExist')
+  //   return isGrowthNotMeasureExist
+  // }
   const renderGrowthNotifcation = () => {
-    console.log(IsGrowthMeasuresForPeriodExist(), "renderGrowthNotifcation");
+    // console.log(IsGrowthMeasuresForPeriodExist(), "renderGrowthNotifcation");
     return (
-      (item.days_from < childAgeInDays && IsGrowthMeasuresForPeriodExist()) ? item.isDeleted ? null :
+      (item.days_from <= childAgeInDays) ? item.isDeleted ? null :
         //if childageInDays is between days_from and days_to then only display notis which have days_from == chilAgeInDays
         (<>
           <NotificationListContainer>
@@ -470,6 +478,7 @@ const NotificationItem = (props: any) => {
       <DividerContainer><Divider></Divider></DividerContainer>
     </>)
   }
+
   const renderCDNotifcation = () => {
     //At the beginning of the period =>cd1
     //5 days before the end of the period =>cd2
@@ -478,7 +487,7 @@ const NotificationItem = (props: any) => {
     // if today;s date childAgeInDays is less= than days_to then only cd2 diplay,
     if (item.title == 'cdNoti1') {
       return (
-        (item.days_from < childAgeInDays) ? item.isDeleted ? null : (<>
+        (item.days_from <= childAgeInDays) ? item.isDeleted ? null : (<>
           <NotificationListContainer>
             <FlexDirRowStart>
               <NotifIcon style={{
@@ -702,15 +711,129 @@ const NotificationItem = (props: any) => {
     }
   }
 
+  const renderVCReminderNotifcation = () => {
+    const luxonLocale = useAppSelector(
+      (state: any) => state.selectedCountry.luxonLocale,
+    );
+    console.log(item, childAgeInDays, "renderVCReminderNotifcation")
+    return (Math.floor(item.days_from) <= childAgeInDays ? item.isDeleted ? null : <>
+      <NotificationListContainer>
+        <FlexDirRowStart>
+          <NotifIcon style={{
+            backgroundColor: item.type == 'vcr' ? vaccinationColor : hcheaderColor
+          }}>
+
+            <Icon
+              name={geticonname(item.type)}
+              size={20}
+              color="#000"
+
+            />
+          </NotifIcon>
+          <NotifiContent>
+            {item.isRead == true ?
+              <Heading4Regular>{t(item.title, {
+                reminderDateTime: formatStringDate(item.periodName, luxonLocale) + "," + formatStringTime(item.growth_period, luxonLocale)
+              })}</Heading4Regular> :
+              <Heading4Bold>{t(item.title, {
+                reminderDateTime: formatStringDate(item.periodName, luxonLocale) + "," + formatStringTime(item.growth_period, luxonLocale)
+              })}</Heading4Bold>
+            }
+
+            <ShiftFromTop5>
+              <Heading6>{
+                getCurrentChildAgeInMonths(
+                  t,
+                  DateTime.fromJSDate(new Date(activeChild.birthDate)).plus({ days: item.days_from })
+                )}</Heading6>
+              {/* <Heading6>{item.days_from},{item.days_to}{"VCR reminder"}</Heading6> */}
+            </ShiftFromTop5>
+            <ShiftFromTop10>
+              <Pressable onPress={() => gotoPage(item.type)}>
+                <ButtonTextSmLineL numberOfLines={2}>{getButtonname(item.type)}</ButtonTextSmLineL>
+              </Pressable></ShiftFromTop10>
+          </NotifiContent>
+
+          <NotifAction>
+            {(isDeleteEnabled === true) ? (
+              <FormOuterCheckbox
+                onPress={() => {
+                  //  console.log(item);
+                  setToggleCheckBox(!toggleCheckBox);
+                  props.onItemChecked(item, !toggleCheckBox)
+                }}>
+                <CheckboxItem>
+                  <View>
+                    {toggleCheckBox ? (
+                      <CheckboxActive>
+                        <Icon name="ic_tick" size={12} color="#000" />
+                      </CheckboxActive>
+                    ) : (
+                      <Checkbox style={{ borderWidth: 1 }}></Checkbox>
+                    )}
+                  </View>
+                </CheckboxItem>
+              </FormOuterCheckbox>
+            ) : (
+              <>
+                <Menu
+                  renderer={renderers.ContextMenu}
+                  style={{
+                    width: 40,
+                    height: 40,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onSelect={(value) =>
+                    console.log(`Selected number: ${value} ${item}`)
+                  }>
+                  <MenuTrigger>
+                    <Icon
+                      style={{
+                        flex: 1,
+                        textAlign: 'right',
+                        alignSelf: 'center',
+                      }}
+                      name={'ic_kebabmenu'}
+                      size={25}
+                      color="#000"
+                    />
+                  </MenuTrigger>
+                  <MenuOptions
+                    customStyles={{
+                      optionsContainer: {
+                        marginTop: 30,
+                        borderRadius: 10,
+                        backgroundColor: primaryTintColor,
+                      },
+
+                      optionWrapper: {
+                        borderBottomWidth: 1,
+                        padding: 15,
+                      },
+
+                    }}>
+                    <MenuOption value={1} onSelect={() => markAsDelete(item)}>
+                      <Heading5Bold>{t('notiOption1')}</Heading5Bold>
+                    </MenuOption>
+                    <MenuOption value={2} onSelect={() => markAsRead(item)}>
+                      <Heading5Bold> {item.isRead == true ? t('notiOption3') : t('notiOption2')}</Heading5Bold>
+                    </MenuOption>
+                  </MenuOptions>
+                </Menu>
+              </>
+            )}
+          </NotifAction>
+
+        </FlexDirRowStart>
+
+      </NotificationListContainer>
+      <DividerContainer><Divider></Divider></DividerContainer>
+    </> : null)
+  }
 
   return (
-    selectedCategories.length == 0 ?
-      (item.type == 'gw' ? renderGrowthNotifcation() : item.type == 'cd' ? renderCDNotifcation() : item.type == 'vc' ? renderVCNotifcation() : item.type == 'hc' ? renderHCNotifcation() : null)
-      :
-      selectedCategories.includes(item.type) ?
-        (item.type == 'gw' ? renderGrowthNotifcation() : item.type == 'cd' ? renderCDNotifcation() : item.type == 'vc' ? renderVCNotifcation() : item.type == 'hc' ? renderHCNotifcation() : null)
-        : null
-
+    (item.type == 'gw' ? renderGrowthNotifcation() : item.type == 'cd' ? renderCDNotifcation() : item.type == 'vc' ? renderVCNotifcation() : item.type == 'hc' ? renderHCNotifcation() : (item.type == 'vcr' || item.type == 'hcr') ? renderVCReminderNotifcation() : null)
   );
 };
 export default NotificationItem;
