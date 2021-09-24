e9ed00a6f1a77360385956a27f09ec7087111c92
prashant-acharekar <prashant.acharekar@datamatics.com>
2021-09-24 11:21:46 +0530

    setting screen toggle ui fixed
    setting screen toggle ui fixed

--- app/components/shared/SettingsStyle.tsx
+++ app/components/shared/SettingsStyle.tsx
@@ -16,3 +16,13 @@ margin:0 -15px;
 
 `;
 
+export const ToggleLabelText = styled.View`
+flex:1;
+margin-top:5px;
+margin-left:8px
+`;
+export const ToggleLabelText1 = styled(ToggleLabelText)`
+
+margin-top:0px;
+margin-left:15px
+`;
