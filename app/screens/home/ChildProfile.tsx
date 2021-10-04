9d19739ea04f8fba335cec68551ab6b766582bc9
prashant-acharekar <prashant.acharekar@datamatics.com>
2021-10-04 14:36:08 +0530

    list scroll UI changes made
    list scroll UI changes made

--- app/screens/home/ChildProfile.tsx
+++ app/screens/home/ChildProfile.tsx
@@ -33,7 +33,7 @@ import {
 import { CHILDREN_PATH } from '@types/types';
 import React, { useContext, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { BackHandler, Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { BackHandler, Dimensions, Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { ThemeContext } from 'styled-components/native';
 import { useAppDispatch, useAppSelector } from '../../../App';
 import { dataRealmCommon } from '../../database/dbquery/dataRealmCommon';
@@ -86,6 +86,7 @@ useEffect(() => {
     "hardwareBackPress",
     backAction,
   );
+ 
 
   return () => {
     backHandler.remove();
@@ -141,6 +142,9 @@ const child_age = useAppSelector(
   // const currentActiveChild =
   //   currentActiveChildId?.length > 0 ? currentActiveChildId[0].value : null;
   // //console.log(currentActiveChild,"..currentActiveChild..");
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
   const SortedchildList = [...childList].sort((a: any, b: any) => {
     if (a.uuid == currentActiveChild) return -1;
   });
@@ -320,7 +324,7 @@ const child_age = useAppSelector(
         <FlexCol>
         <AreaContainer>
           <View style={{flexDirection: 'column'}}>
-            <ScrollView style={{maxHeight:'55%',height:'auto'}} nestedScrollEnabled={true}>
+            <ScrollView style={{maxHeight:windowHeight-350,height:'auto'}} nestedScrollEnabled={true}>
               {SortedchildList.length > 0
                 ? SortedchildList.map((item: any, index: number) => {
                    console.log(item,"..item..");
