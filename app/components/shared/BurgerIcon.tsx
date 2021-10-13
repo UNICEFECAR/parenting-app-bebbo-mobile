0f505ccba3210b3a0c1a867b9fef0c3fe744785c
prashant-acharekar <prashant.acharekar@datamatics.com>
2021-10-13 15:40:14 +0530

    Minor UI changes in Child Dev, Header Dropdown, Loading Screen, UserGuide screen.
    Minor UI changes in Child Dev, Header Dropdown, Loading Screen, UserGuide screen.

--- app/components/shared/BurgerIcon.tsx
+++ app/components/shared/BurgerIcon.tsx
@@ -2,15 +2,15 @@ import Icon from '@components/shared/Icon';
 import { DrawerActions, useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Pressable } from 'react-native';
-import { HeaderIconView } from './HeaderContainerStyle';
+import { HeaderIconView,HeaderBurgerPress } from './HeaderContainerStyle';
 const BurgerIcon = (props:any) => {
   const navigation = useNavigation();
   return (
     <HeaderIconView>
-      <Pressable
+      <HeaderBurgerPress
         onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
         <Icon name="ic_navmenu" size={15} color={props.color || '#FFF'} />
-      </Pressable>
+      </HeaderBurgerPress>
     </HeaderIconView>
   );
 };
