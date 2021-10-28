20061bb0ffee92a7e206e97ccb3a2c7bad17c5ec
NehaRuparelgit <neha.ruparel@datamatics.com>
2021-10-28 14:44:35 +0530

    RTL support added
    - RTL support added for testing on Serbia English.
    - Restart plugin added as required.
    - Made changes in Navigation as needed.
    - Made changes in Android native files for RTL support.

--- android/app/src/main/java/com/parentbuddyapp/MainApplication.java
+++ android/app/src/main/java/com/parentbuddyapp/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -44,6 +45,9 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.allowRTL(getApplicationContext(), true);
+    // sharedI18nUtilInstance.forceRTL(getApplicationContext(), true);
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
