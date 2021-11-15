5ff29113d4777a2672f49e09bcf9fb4f74a09b24
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-11-15 23:09:40 +0530

    fiveYearsGreater string added
    fiveYearsGreater string added

--- app/assets/translations/appConstants/KGky.ts
+++ app/assets/translations/appConstants/KGky.ts
@@ -397,6 +397,7 @@ const KGky = {
                 addChildText: "Балаңыз жөнүндө маалыматтарды кошуңуз",
                 notiTimeAgoTxt: "ago",
                 todayTxt:"today",
+                fiveYearsGreater:"Standard growth values are available only for children 0-5 years old and therefore growth charts are not applicable for children older than 5 years old"
         }
 }
 export default KGky;
