e3ad2dcec0f4cf520e79d0cc05c598f66eb08408
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-12-01 11:26:18 +0530

    changes for key next
    changes for key next

--- app/assets/translations/appConstants/ALsq.ts
+++ app/assets/translations/appConstants/ALsq.ts
@@ -401,7 +401,10 @@ const ALsq = {
               forceUpdatePopupTitle:"Information",
               forceUpdatePopupText:"Data will be updated",
               forceUpdateOkBtn:"OK",
-              favScreenremoveText:"Remove"
+              favScreenremoveText:"Remove",
+              settingScreenimportOptionHeader: "Import data on",
+              childSetupprematureMessageNext: "The content of this section is displayed according to the corrected and not the actual age of your prematurely born child",
+     
        }
 }
 export default ALsq;
