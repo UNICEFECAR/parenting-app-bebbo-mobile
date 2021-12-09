bf61794349551469f9e38ba60c79cd84c22af27b
NehaRuparelgit <neha.ruparel@datamatics.com>
2021-12-09 11:54:16 +0530

    Chatbot design and icon changes
    - Chatbot design and icon changes

---
+++ app/components/shared/SupportChatStyle.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+const ChatContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  position:relative;
+  z-index:1;
+`;
+export default ChatContainer;
+
+export const ChatBgImage = styled.View`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-color:#dbe9f6;
+`;
