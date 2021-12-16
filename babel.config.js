e1743f7f6df49bd9a9d857167ac24393cec1e609
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2021-12-16 18:08:12 +0530

    changes for issues
    changes for issues

--- babel.config.js
+++ babel.config.js
@@ -42,6 +42,7 @@ module.exports = (api) => {
   if (babelEnv !== 'development') {
     plugins.push(['transform-remove-console', {exclude: ['error', 'warn']}]);
   }
+  // plugins.push(["react-native-reanimated/plugin"]);
   return {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: plugins,
