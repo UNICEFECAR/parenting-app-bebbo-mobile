76babcbf8cd6632f840ab9eb63e12aaab667ae25
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2022-01-11 10:36:08 +0530

    changess for downloadAllPopupTitle,downloadAllPopupText key
    changess for downloadAllPopupTitle,downloadAllPopupText key

--- app/assets/translations/appConstants/GRarb.ts
+++ app/assets/translations/appConstants/GRarb.ts
@@ -430,8 +430,8 @@ const GRarb = {
                 importBtntxt: "Import File",
 
                 //27/12/2021
-                downloadAllPopupTitle: "Update data",
-                downloadAllPopupText: "This will take time. Do you want to download all data now?",
+                downloadAllPopupTitle: "Download full data",
+                downloadAllPopupText: "This will download entire app content and will take time and more than 200MB data. We recommend doing this on Wi-Fi connection. Do you want to download all data now?",
                 downloadAllCancelPopUpBtn: "Cancel",
                 downloadAllContinueBtn: "Continue",
                 downloadAllSuccessPopupTitle: "Success",
