cbb7bfe6d8e78e6a4c00bcf84b7a90960255743d
NehaRuparelgit <neha.ruparel@datamatics.com>
2022-02-07 12:07:37 +0530

    Changes
    - logo path changes on different screens
    - Unicef logo shown on first loading in Kosovo build

--- app/screens/localization/CountryLanguageConfirmation.tsx
+++ app/screens/localization/CountryLanguageConfirmation.tsx
@@ -36,7 +36,7 @@ import { BackHandler } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeContext } from 'styled-components/native';
 import { useAppDispatch, useAppSelector } from '../../../App';
-import { allApisObject, appConfig, buildFor, buildForFoleja } from '../../assets/translations/appOfflineData/apiConstants';
+import { allApisObject, appConfig } from '../../assets/translations/appOfflineData/apiConstants';
 import { onLocalizationSelect, setAppLayoutDirection, setAppLayoutDirectionParams, setAppLayoutDirectionScreen, setrestartOnLangChange, setSponsorStore } from '../../redux/reducers/localizationSlice';
 import { setInfoModalOpened } from '../../redux/reducers/utilsSlice';
 import RNRestart from 'react-native-restart';
