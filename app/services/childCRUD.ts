502fe4ac1a249d6c419bada04e083682292ee214
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2022-04-01 13:19:07 +0530

    changes for fb sdk build
    changes for fb sdk build

--- app/services/childCRUD.ts
+++ app/services/childCRUD.ts
@@ -582,7 +582,7 @@ export const addChild = async (languageCode: any, editScreen: boolean, param: nu
     let createresult = await userRealmCommon.create<ChildEntity>(ChildEntitySchema, data);
     // dispatch(setActiveChildData(data[0]));
   }
-
+  //new child add from 
   if (param == 0) {
     // navigation.reset({
     //   index: 0,
@@ -605,6 +605,7 @@ export const addChild = async (languageCode: any, editScreen: boolean, param: nu
     let userEnteredChildData = await dataRealmCommon.updateSettings<ConfigSettingsEntity>(ConfigSettingsSchema, "userEnteredChildData", "true");
     setActiveChild(languageCode, data[0].uuid, dispatch, child_age);
   }
+  //child add from add sibling
   else if (param == 1) {
     let currentActiveChildId = await dataRealmCommon.getFilteredData<ConfigSettingsEntity>(ConfigSettingsSchema, "key='currentActiveChildId'");
     if (currentActiveChildId?.length > 0) {
@@ -616,6 +617,7 @@ export const addChild = async (languageCode: any, editScreen: boolean, param: nu
     navigation.navigate('ChildSetupList');
     // setActiveChild(data[0].uuid,dispatch,child_age);
   }
+  //child add from edit/add expecting
   else {
     let currentActiveChildId = await dataRealmCommon.getFilteredData<ConfigSettingsEntity>(ConfigSettingsSchema, "key='currentActiveChildId'");
     let ageLimit = [];
