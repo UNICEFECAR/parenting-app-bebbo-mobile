4bef206b9ed77717192ed1f9cbffa5037c61ce9a
NehaRuparelgit <neha.ruparel@datamatics.com>
2022-04-08 13:47:19 +0530

    Notification issues solved
    - New date columns added after import code.
    - Solved notification creation issue for expected child on import.Changed condition in service file.
    - Added new strings where required.
    - Solved datetime issue for 2nd reminder notification

--- app/assets/translations/appConstants/bebbo/BYru.ts
+++ app/assets/translations/appConstants/bebbo/BYru.ts
@@ -442,6 +442,8 @@ const BYru = {
                 hcrNoti2: "Медосмотр запланирован на {{reminderDateTime}}",
                 vcrNoti3: "Вакцинация запланирована на {{reminderDateTime}}, пожалуйста, введите данные о вакцине",
                 hcrNoti3: "Медосмотр запланирован на {{reminderDateTime}}, пожалуйста, введите данные о медосмотре",
+                hcHasScheduled: "Health-check is scheduled",
+                vcHasScheduled: "Vaccination is scheduled",
         }
 }
 export default BYru;
