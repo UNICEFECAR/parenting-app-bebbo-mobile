586274ebcb221b54dcd847e9214eb4fea551fbe0
Kalyani790 <82521546+Kalyani790@users.noreply.github.com>
2022-07-14 10:46:14 +0530

    local notification redirect code
    local notification redirect code

--- app/navigation/AppNavigation.tsx
+++ app/navigation/AppNavigation.tsx
@@ -46,7 +46,7 @@ import messaging from '@react-native-firebase/messaging';
 import {AppEventsLogger, Settings} from 'react-native-fbsdk-next';
 import {PERMISSIONS, RESULTS, request, check} from 'react-native-permissions';
 import LocalNotifications from '../services/LocalNotifications';
-
+import PushNotification from 'react-native-push-notification';
 // import {ThemeProvider} from 'styled-components/native';
 // import {useSelector} from 'react-redux';
 const RootStack = createStackNavigator<RootStackParamList>();
@@ -237,12 +237,44 @@ export default () => {
       
     // }, 6000);
   }, []);
-  const handleEvent=(data:any)=>{
-    console.log("recieveenet",data)
+  const createLocalNotificationListeners = async () => {
+    try {
+      PushNotification.configure({
+  // this will listen to your local push notifications on clicked 
+        onNotification: (notification:any) => {
+          console.log(notification,"..11notification11..");
+          if(notification && notification.data && notification.data.screen!='' && notification.data.screen!=null && notification.data.screen!=undefined && userIsOnboarded){
+            navigationRef.current?.navigate("Tools", { screen: notification.data.screen })
+
+          }
+        },
+        popInitialNotification: true,
+        requestPermissions: true,
+      });
+      PushNotification.popInitialNotification((notification:any) => {
+        console.log(notification,"..22notification22..");
+        if(notification && notification.data && notification.data.screen!='' && notification.data.screen!=null && notification.data.screen!=undefined && userIsOnboarded){
+          navigationRef.current?.navigate("Tools", { screen: notification.data.screen })
+
+        }
+        // this will listen to your local push notifications on opening app from background state
+          
+      })
+    
+    } catch (e) {
+      // alert(e)
+     console.log("error")
+    }
   }
   useEffect(() => {
-    // const eventListener = DeviceEventEmitter.addListener('notiTap',handleEvent); 
-    // console.log(eventListener,"..eventListener..")
+   // let notiListener=null;
+   createLocalNotificationListeners();
+    // return () => {
+    //   notiListener=null;
+    // }
+    }, []);
+  useEffect(() => {
+    
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       // console.log("msg----",remoteMessage);
       if(remoteMessage && remoteMessage.notification && remoteMessage.notification.body && remoteMessage.notification.title) {
@@ -349,6 +381,17 @@ export default () => {
   useEffect(() => {
     console.log("linkedURL3---",linkedURL);
   }, [linkedURL]);
+ 
+  useEffect(() => {
+    let unsubscribe:any=null;
+    // async function createLocalNotification(){
+    //  unsubscribe  = createLocalNotificationListeners().then(r => console.log("local push notification listeners created"));
+    // }
+    // createLocalNotification();
+    // return () => {
+    // unsubscribe=null;
+    // }
+  }, []);
   useEffect(() => {
     if (userIsOnboarded == true) {
       // console.log("calculated");
