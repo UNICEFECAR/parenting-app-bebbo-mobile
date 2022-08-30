99b4ac2543fa82b5396fb1192d5da1979863cc43
NehaRuparelgit <82521889+NehaRuparelgit@users.noreply.github.com>
2022-08-30 16:24:44 +0530

    changes
    changes

--- app/components/shared/FormPrematureContainer.tsx
+++ app/components/shared/FormPrematureContainer.tsx
@@ -16,6 +16,6 @@ export const FormInfoPress = styled.Pressable`
   padding: 10px 10px;
 `;
 export const FormDobInfoPress = styled.Pressable`
-padding: 0px 10px;
+padding: 10px 10px;
 `;
 export default FormPrematureContainer;
