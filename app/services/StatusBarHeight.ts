4e9cf622ab345f9ebd07a5e1a729bb5cb08882a1
NehaRuparelgit <82521889+NehaRuparelgit@users.noreply.github.com>
2022-09-02 11:41:42 +0530

    minor changes
    minor changes

--- app/services/StatusBarHeight.ts
+++ app/services/StatusBarHeight.ts
@@ -20,38 +20,38 @@ const IP12MAX_HEIGHT = 926;
 const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get("window");
 
 let statusBarHeight = STATUSBAR_DEFAULT_HEIGHT;
-let isIPhoneX_v = false;
-let isIPhoneXMax_v = false;
-let isIPhone12_v = false;
-let isIPhone12Max_v = false;
-let isIPhoneWithMonobrow_v = false;
+let isIPhoneXv = false;
+let isIPhoneXMaxv = false;
+let isIPhone12v = false;
+let isIPhone12Maxv = false;
+let isIPhoneWithMonobrowv = false;
 
 if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
   if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhoneX_v = true;
+    isIPhoneWithMonobrowv = true;
+    isIPhoneXv = true;
     statusBarHeight = STATUSBAR_X_HEIGHT;
   } else if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhoneXMax_v = true;
+    isIPhoneWithMonobrowv = true;
+    isIPhoneXMaxv = true;
     statusBarHeight = STATUSBAR_X_HEIGHT;
   } else if (W_WIDTH === IP12_WIDTH && W_HEIGHT === IP12_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhone12_v = true;
+    isIPhoneWithMonobrowv = true;
+    isIPhone12v = true;
     statusBarHeight = STATUSBAR_IP12_HEIGHT;
   } else if (W_WIDTH === IP12MAX_WIDTH && W_HEIGHT === IP12MAX_HEIGHT) {
-    isIPhoneWithMonobrow_v = true;
-    isIPhone12Max_v = true;
+    isIPhoneWithMonobrowv = true;
+    isIPhone12Maxv = true;
     statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
   }
 }
 
-export const isIPhoneX = ():any => isIPhoneX_v;
-export const isIPhoneXMax = ():any => isIPhoneXMax_v;
-export const isIPhone12 = ():any => isIPhone12_v;
-export const isIPhone12Max = ():any => isIPhone12Max_v;
-export const isIPhoneWithMonobrow = ():any => isIPhoneWithMonobrow_v;
-export function getStatusBarHeight(skipAndroid:any):any {
+export const isIPhoneX = (): any => isIPhoneXv;
+export const isIPhoneXMax = (): any => isIPhoneXMaxv;
+export const isIPhone12 = (): any => isIPhone12v;
+export const isIPhone12Max = (): any => isIPhone12Maxv;
+export const isIPhoneWithMonobrow = (): any => isIPhoneWithMonobrowv;
+export function getStatusBarHeight(skipAndroid: any): any {
   return Platform.select({
     ios: statusBarHeight,
     android: skipAndroid ? 0 : StatusBar.currentHeight,
