02f0c5bb030fcd8bc524004b4f6f21b9670e4c59
neha.ruparel@datamatics.com <neha.ruparel@datamatics.com>
2022-11-09 13:02:21 +0530

    changes for name
    changes for name

--- app/services/LocalNotifications.js
+++ app/services/LocalNotifications.js
@@ -43,7 +43,7 @@ class LocalNotifications {
     const notificationid = String(notiId).length > 9 ? String(notiId).substr(String(notiId).length-9) : String(notiId);
     PushNotification.cancelLocalNotification(notificationid);
   }
-  removeReminderLocalNotification() {
+  removeAllDeliveredLocalNotifications() {
    // Alert.alert("removed","removed")
     PushNotification.removeAllDeliveredNotifications();
    // Alert.alert("11removed","11removed")
