b9016bc92558cec2b0c0365043fb788fa17adf8a
kalyani.kaushik@datamatics.com <kalyani.kaushik@datamatics.com>
2022-12-19 14:49:09 +0530

    removing of 2 apis from onboarding
    removing of child development pinned contents apis from onboarding

--- app/assets/translations/appOfflineData/apiConstants.ts
+++ app/assets/translations/appOfflineData/apiConstants.ts
@@ -1,280 +1,260 @@
 import { apiUrlDevelop } from "react-native-dotenv";
 import RNFS from 'react-native-fs';
-export const destinationFolder=RNFS.DocumentDirectoryPath + '/content/';
+export const destinationFolder = RNFS.DocumentDirectoryPath + '/content/';
 export const buildForFoleja = 'foleja'; //'foleja'
 export const buildForBebbo = 'bebbo'; //'foleja'
 export const buildFor = buildForBebbo; //'foleja'
 export const maxRelatedArticleSize = 3;
 export const isArticlePinned = '1';
 export const articleCategory = '4,1,55,56,3,2';
-export const articleCategoryArray = [4,1,55,56,3,2];
-export const regexpEmojiPresentation = /[^\p{L} ]/gu;    
-export const luxonDefaultLocale="en-US";                   
+export const articleCategoryArray = [4, 1, 55, 56, 3, 2];
+export const regexpEmojiPresentation = /[^\p{L} ]/gu;
+export const luxonDefaultLocale = "en-US";
 export const videoTypeVimeo = "vimeo";
 export const videoTypeYoutube = "youtube";
 export const videoTypeImage = "novideo";
-export const backupGDriveFolderName= 'ParentBuddy';
-export const backupGDriveFileName= 'my.backup';
-export const tempRealmFile=RNFS.DocumentDirectoryPath + '/' + 'user1.realm';
+export const backupGDriveFolderName = 'ParentBuddy';
+export const backupGDriveFileName = 'my.backup';
+export const tempRealmFile = RNFS.DocumentDirectoryPath + '/' + 'user1.realm';
 export const backUpPath = RNFS.DocumentDirectoryPath + '/my.backup';
 export const tempbackUpPath = RNFS.TemporaryDirectoryPath + 'my.backup';
 export const firstPeriodicSyncDays = 7;
 export const secondPeriodicSyncDays = 30;
-export const shareText=(String(buildFor) != buildForBebbo)?'\nhttps://www.bebbo.app/foleja/share/':'\nhttps://www.bebbo.app/share/';
-export const shareTextButton=(String(buildFor) != buildForBebbo)?'https://www.bebbo.app/foleja/share/':'https://www.bebbo.app/share/';
-export const maleData: any={
-    "id": 37,
-    "name": "Male",
-    "unique_name": "male"
+export const shareText = (String(buildFor) != buildForBebbo) ? '\nhttps://www.bebbo.app/foleja/share/' : '\nhttps://www.bebbo.app/share/';
+export const shareTextButton = (String(buildFor) != buildForBebbo) ? 'https://www.bebbo.app/foleja/share/' : 'https://www.bebbo.app/share/';
+export const maleData: any = {
+  "id": 37,
+  "name": "Male",
+  "unique_name": "male"
 };
-export const relationShipMotherId=109801;
-export const relationShipFatherId=109806;
-export const relationShipOtherCaregiverId=109811;
-export const relationShipServiceProviderId=109816;
-export const femaleData: any={
-    "id": 38,
-    "name": "Female",
-    "unique_name": "female"
+export const relationShipMotherId = 109801;
+export const relationShipFatherId = 109806;
+export const relationShipOtherCaregiverId = 109811;
+export const relationShipServiceProviderId = 109816;
+export const femaleData: any = {
+  "id": 38,
+  "name": "Female",
+  "unique_name": "female"
 };
-export const bothParentGender=60;
-export const bothChildGender=59;
-export const girlChildGender=41;
-export const boyChildGender=40;
-export const weightGrowthType=32786;
-export const heightGrowthType=6461;
+export const bothParentGender = 60;
+export const bothChildGender = 59;
+export const girlChildGender = 41;
+export const boyChildGender = 40;
+export const weightGrowthType = 32786;
+export const heightGrowthType = 6461;
 const today = new Date();
 today.setFullYear(today.getFullYear() + 5);
-export const  fiveYearFromNow = today;
+export const fiveYearFromNow = today;
 export const restOfTheWorldCountryId = 126;
 export const videoArticleMandatory = 0;
-export const maxArticleSize = 5; 
+export const maxArticleSize = 5;
 export const appConfig = {
   articles: 'articles',
   videoArticles: 'video-articles',
   dailyMessages: 'daily-homescreen-messages',
   basicPages: 'basic-pages',
-  sponsors:'sponsors',
-  taxonomies:'taxonomies',
-  standardDeviation:'standard_deviation',
-  milestones:'milestones',
-  activities:'activities',
-  surveys:'surveys',
-  childDevelopmentData:'child-development-data',
-  childGrowthData:'child-growth-data',
-  vaccinations:'vaccinations',
-  healthCheckupData:'health-checkup-data',
-  pinnedContent:'pinned-contents',
-  vaccinePinnedContent:'pinnedvaccinations',
-  childdevGirlPinnedContent:'child_development/'+girlChildGender,
-  childdevBoyPinnedContent:'child_development/'+boyChildGender,
-  childGrowthPinnedContent:'child_growth',
-  healthcheckupPinnedContent:'health_check_ups',
-  faqPinnedContent:'faq',
-  faqUpdatedPinnedContent:'updatedfaq',
-  milestoneRelatedArticle:'milestonerelatedarticle',
-  checkUpdate:'check-update',
-  faqs:'faqs',
-  archive:'archive',
+  sponsors: 'sponsors',
+  taxonomies: 'taxonomies',
+  standardDeviation: 'standard_deviation',
+  milestones: 'milestones',
+  activities: 'activities',
+  surveys: 'surveys',
+  childDevelopmentData: 'child-development-data',
+  childGrowthData: 'child-growth-data',
+  vaccinations: 'vaccinations',
+  healthCheckupData: 'health-checkup-data',
+  pinnedContent: 'pinned-contents',
+  vaccinePinnedContent: 'pinnedvaccinations',
+  childGrowthPinnedContent: 'child_growth',
+  healthcheckupPinnedContent: 'health_check_ups',
+  faqPinnedContent: 'faq',
+  faqUpdatedPinnedContent: 'updatedfaq',
+  milestoneRelatedArticle: 'milestonerelatedarticle',
+  checkUpdate: 'check-update',
+  faqs: 'faqs',
+  archive: 'archive',
 }
-export const finalUrl = (apiEndpoint: string,selectedCountry: number | undefined,selectedLang: string): any=>{
-    if(apiEndpoint==appConfig.sponsors){
-        return apiUrlDevelop+ '/'+apiEndpoint+'/'+selectedCountry;
-    } 
-    if(apiEndpoint==appConfig.taxonomies){
-        return apiUrlDevelop+ '/'+apiEndpoint+'/'+selectedLang+'/all';
-    }
-    if(apiEndpoint==appConfig.checkUpdate){
-        return apiUrlDevelop+ '/'+apiEndpoint+'/'+selectedCountry;
-    }
-    if(apiEndpoint==appConfig.vaccinePinnedContent){
-        return apiUrlDevelop+ '/pinned-contents/'+selectedLang+'/vaccinations';
-    }
-    if(apiEndpoint==appConfig.childGrowthPinnedContent){
-        return apiUrlDevelop+ '/pinned-contents/'+selectedLang+'/'+apiEndpoint;
-    }
-    if(apiEndpoint==appConfig.childdevGirlPinnedContent){
-        return apiUrlDevelop+ '/pinned-contents/'+selectedLang+'/'+apiEndpoint;
-    }
-    if(apiEndpoint==appConfig.childdevBoyPinnedContent){
-        return apiUrlDevelop+ '/pinned-contents/'+selectedLang+'/'+apiEndpoint;
-    }
-    if(apiEndpoint==appConfig.healthcheckupPinnedContent){
-        return apiUrlDevelop+ '/pinned-contents/'+selectedLang+'/'+apiEndpoint;
-    }
-    if(apiEndpoint==appConfig.faqPinnedContent){
-        return apiUrlDevelop+ '/pinned-contents/'+selectedLang+'/'+apiEndpoint;
-    }
-    if(apiEndpoint==appConfig.milestoneRelatedArticle){
-        return apiUrlDevelop+ '/related-article-contents/'+selectedLang+'/milestone';
-    }
-    if(apiEndpoint==appConfig.faqUpdatedPinnedContent){
-      return apiUrlDevelop+ '/updated-pinned-contents/'+selectedLang+'/faq';
+export const finalUrl = (apiEndpoint: string, selectedCountry: number | undefined, selectedLang: string): any => {
+  if (apiEndpoint == appConfig.sponsors) {
+    return apiUrlDevelop + '/' + apiEndpoint + '/' + selectedCountry;
   }
-    return apiUrlDevelop+ '/'+apiEndpoint+'/'+selectedLang;
+  if (apiEndpoint == appConfig.taxonomies) {
+    return apiUrlDevelop + '/' + apiEndpoint + '/' + selectedLang + '/all';
+  }
+  if (apiEndpoint == appConfig.checkUpdate) {
+    return apiUrlDevelop + '/' + apiEndpoint + '/' + selectedCountry;
+  }
+  if (apiEndpoint == appConfig.vaccinePinnedContent) {
+    return apiUrlDevelop + '/pinned-contents/' + selectedLang + '/vaccinations';
+  }
+  if (apiEndpoint == appConfig.childGrowthPinnedContent) {
+    return apiUrlDevelop + '/pinned-contents/' + selectedLang + '/' + apiEndpoint;
+  }
+  if (apiEndpoint == appConfig.healthcheckupPinnedContent) {
+    return apiUrlDevelop + '/pinned-contents/' + selectedLang + '/' + apiEndpoint;
+  }
+  if (apiEndpoint == appConfig.faqPinnedContent) {
+    return apiUrlDevelop + '/pinned-contents/' + selectedLang + '/' + apiEndpoint;
+  }
+  if (apiEndpoint == appConfig.milestoneRelatedArticle) {
+    return apiUrlDevelop + '/related-article-contents/' + selectedLang + '/milestone';
+  }
+  if (apiEndpoint == appConfig.faqUpdatedPinnedContent) {
+    return apiUrlDevelop + '/updated-pinned-contents/' + selectedLang + '/faq';
+  }
+  return apiUrlDevelop + '/' + apiEndpoint + '/' + selectedLang;
 }
 
 
-  export const allApisObject = (isDatetimeReq: any, dateTimeObj: any): any => {
-    const allApiObject = [
-      {
-        apiEndpoint: appConfig.sponsors,
-        method: 'get',
-        postdata: {},
-        saveinDB: false,
-      },
-      {
-        apiEndpoint: appConfig.taxonomies,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.basicPages,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.videoArticles,
-        method: 'get',
-        postdata: isDatetimeReq == true && dateTimeObj['videoArticlesDatetime'] != '' ? {datetime: dateTimeObj['videoArticlesDatetime']} : {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.dailyMessages,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.activities,
-        method: 'get',
-        postdata: isDatetimeReq == true && dateTimeObj['activitiesDatetime'] != '' ? {datetime: dateTimeObj['activitiesDatetime']} : {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.surveys,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.milestones,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.childDevelopmentData,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.vaccinations,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.healthCheckupData,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.vaccinePinnedContent,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.childGrowthPinnedContent,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.childdevGirlPinnedContent,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.childdevBoyPinnedContent,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.healthcheckupPinnedContent,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.faqPinnedContent,
-        method: 'get',
-        postdata: isDatetimeReq == true && dateTimeObj['faqPinnedContentDatetime'] != '' ? {datetime: dateTimeObj['faqPinnedContentDatetime']} : {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.milestoneRelatedArticle,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.standardDeviation,
-        method: 'get',
-        postdata: {},
-        saveinDB: true,
-      },
-      {
-        apiEndpoint: appConfig.faqs,
-        method: 'get',
-        postdata: isDatetimeReq == true && dateTimeObj['faqsDatetime'] != '' ? {datetime: dateTimeObj['faqsDatetime']} : {},
-        saveinDB: true,
-      }
-    ];
-    if(isDatetimeReq == true) {
-      allApiObject.push({
-        apiEndpoint: appConfig.faqUpdatedPinnedContent,
-        method: 'get',
-        postdata: isDatetimeReq == true &&  dateTimeObj['faqUpdatedPinnedContentDatetime'] != '' ? {datetime: dateTimeObj['faqUpdatedPinnedContentDatetime']} : dateTimeObj['faqPinnedContentDatetime'] != '' ? {datetime: dateTimeObj['faqPinnedContentDatetime']} : {},
-        saveinDB: true,
-      },
+export const allApisObject = (isDatetimeReq: any, dateTimeObj: any): any => {
+  const allApiObject = [
+    {
+      apiEndpoint: appConfig.sponsors,
+      method: 'get',
+      postdata: {},
+      saveinDB: false,
+    },
+    {
+      apiEndpoint: appConfig.taxonomies,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.basicPages,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.videoArticles,
+      method: 'get',
+      postdata: isDatetimeReq == true && dateTimeObj['videoArticlesDatetime'] != '' ? { datetime: dateTimeObj['videoArticlesDatetime'] } : {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.dailyMessages,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.activities,
+      method: 'get',
+      postdata: isDatetimeReq == true && dateTimeObj['activitiesDatetime'] != '' ? { datetime: dateTimeObj['activitiesDatetime'] } : {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.surveys,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.milestones,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.childDevelopmentData,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.vaccinations,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.healthCheckupData,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.vaccinePinnedContent,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.childGrowthPinnedContent,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.healthcheckupPinnedContent,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.faqPinnedContent,
+      method: 'get',
+      postdata: isDatetimeReq == true && dateTimeObj['faqPinnedContentDatetime'] != '' ? { datetime: dateTimeObj['faqPinnedContentDatetime'] } : {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.milestoneRelatedArticle,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.standardDeviation,
+      method: 'get',
+      postdata: {},
+      saveinDB: true,
+    },
+    {
+      apiEndpoint: appConfig.faqs,
+      method: 'get',
+      postdata: isDatetimeReq == true && dateTimeObj['faqsDatetime'] != '' ? { datetime: dateTimeObj['faqsDatetime'] } : {},
+      saveinDB: true,
+    }
+  ];
+  if (isDatetimeReq == true) {
+    allApiObject.push({
+      apiEndpoint: appConfig.faqUpdatedPinnedContent,
+      method: 'get',
+      postdata: isDatetimeReq == true && dateTimeObj['faqUpdatedPinnedContentDatetime'] != '' ? { datetime: dateTimeObj['faqUpdatedPinnedContentDatetime'] } : dateTimeObj['faqPinnedContentDatetime'] != '' ? { datetime: dateTimeObj['faqPinnedContentDatetime'] } : {},
+      saveinDB: true,
+    },
       {
         apiEndpoint: appConfig.archive,
         method: 'get',
-        postdata: isDatetimeReq == true &&  dateTimeObj['archiveDatetime'] != '' ? {datetime: dateTimeObj['archiveDatetime']} : dateTimeObj['faqPinnedContentDatetime'] != '' ? {datetime: dateTimeObj['faqPinnedContentDatetime']} : {},
+        postdata: isDatetimeReq == true && dateTimeObj['archiveDatetime'] != '' ? { datetime: dateTimeObj['archiveDatetime'] } : dateTimeObj['faqPinnedContentDatetime'] != '' ? { datetime: dateTimeObj['faqPinnedContentDatetime'] } : {},
         saveinDB: true,
       })
-    }
-    return allApiObject;
   }
+  return allApiObject;
+}
 export const articleCategoryobj = [
-    {name:'playingAndLearning',id:55, image:'ic_artl_play'},
-    {name:'healthAndWellbeingid',id:2, image:'ic_artl_health'},
-    {name:'safetyAndProtection', id:3, image:'ic_artl_safety'},
-    {name:'responsiveParenting', id:56,image:'ic_artl_responsive'},
-    {name:'parentingCorner', id:4,image:'ic_artl_parenting'},
-    {name:'nutritionAndBreastfeeding',id:1, image:'ic_artl_nutrition'},
+  { name: 'playingAndLearning', id: 55, image: 'ic_artl_play' },
+  { name: 'healthAndWellbeingid', id: 2, image: 'ic_artl_health' },
+  { name: 'safetyAndProtection', id: 3, image: 'ic_artl_safety' },
+  { name: 'responsiveParenting', id: 56, image: 'ic_artl_responsive' },
+  { name: 'parentingCorner', id: 4, image: 'ic_artl_parenting' },
+  { name: 'nutritionAndBreastfeeding', id: 1, image: 'ic_artl_nutrition' },
 ]
 export const activityCategoryobj = [
-    {name:'Socio-emotional',id:6431, image:'ic_act_emotional'},
-    {name:'Language and communication', id:6441,image:'ic_act_language'},
-    {name:'Cognitive', id:6436, image:'ic_act_cognitive'},
-    {name:'Motor',id:6421, image:'ic_act_movement'},
+  { name: 'Socio-emotional', id: 6431, image: 'ic_act_emotional' },
+  { name: 'Language and communication', id: 6441, image: 'ic_act_language' },
+  { name: 'Cognitive', id: 6436, image: 'ic_act_cognitive' },
+  { name: 'Motor', id: 6421, image: 'ic_act_movement' },
 ]
 export const basicPagesUniqueName = {
-    aboutus: 'about_us',
-    terms: 'terms_and_conditions',
-    privacypolicy: 'privacy_policy',
+  aboutus: 'about_us',
+  terms: 'terms_and_conditions',
+  privacypolicy: 'privacy_policy',
 
 }
-export const measurementPlaces = (items: any): any=>{
-    return [ {id:0,title:items[0] },{id:1,title:items[1] }]
+export const measurementPlaces = (items: any): any => {
+  return [{ id: 0, title: items[0] }, { id: 1, title: items[1] }]
 }
 export const maxCharForRemarks = 200;
 export const threeeMonthDays = 90;
