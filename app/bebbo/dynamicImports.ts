27b003fdb09cc3e03587606c873f1aa96d0b9425
kalyani.kaushik@datamatics.com <kalyani.kaushik@datamatics.com>
2023-01-04 11:20:57 +0530

    translation for en added,image link added
    translation for en added,image link added

--- app/bebbo/dynamicImports.ts
+++ app/bebbo/dynamicImports.ts
@@ -4,6 +4,8 @@ import bebboLogoShape1 from '@assets/svg/bebbo/bebbo_logo_shape.svg';
 export const bebboLogoShape=bebboLogoShape1;
 import imgLogoChatbotNew1 from '@assets/svg/bebbo/img-logo-chatbot-new.svg';
 export const imgLogoChatbotNew=imgLogoChatbotNew1;
+import menubebboLogo from '@assets/svg/bebbo/bebbomenulogo.svg';
+export const menubebboLogoNew=menubebboLogo;
 export const AVAILABLE_LANGUAGES =require('../localization/Available_lang_bebbo').default;
 export const taxonomydata =require('@offlinedata/bebbo/taxonomies').taxonomydata;
 export const ActivitiesData =require('@offlinedata/bebbo/ActivitiesData').ActivitiesData;
