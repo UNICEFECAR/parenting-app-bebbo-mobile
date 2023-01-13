fcc945ece42b937731668571f2a892a2704aee5e
kalyani.kaushik@datamatics.com <kalyani.kaushik@datamatics.com>
2023-01-13 15:21:07 +0530

    translation added
    translation added

--- app/assets/translations/appConstants/bebbo/MKmk.ts
+++ app/assets/translations/appConstants/bebbo/MKmk.ts
@@ -449,6 +449,8 @@ const MKmk = {
                 donateButton: "Донирај за Беббо",
                 contactUs: "Испрати ни порака",
                 aboutBebboDrawerMenu: "За {{appName}}",
+                tNcTermsConditions: "условите за користење на апликацијата Bebbo",
+                tNcStaticContent: "Беббо не собира ниту складира никакви лични податоци. Сите податоци што ги внесувате се зачувуваат само на вашиот телефон.",
         }
 }
 export default MKmk;
