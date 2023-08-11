c74067753500df83450ef1d015ce4764c7365e88
DivyaniPatel96 <divyani.patel@datamatics.com>
2023-08-11 16:23:34 +0530

    Work on Story ID - 168504 : Improve Search of Articles
    Work on Improve search results for articles and highlight word on details screen

--- app/screens/home/bottomTabs/Articles.tsx
+++ app/screens/home/bottomTabs/Articles.tsx
@@ -88,41 +88,52 @@ const Articles = ({ route, navigation }: any): any => {
   const modalScreenText = 'articleModalText';
   const netInfoval = useNetInfoHook();
   
-  const mergearr = (articlearrold: any[],videoartarrold: any[]): any => {
-    let combinedarr: any[] = [];
-    let i = 0;
-    let j = 0;
-    const articlearr = randomArrayShuffle(articlearrold)
-    const videoartarr = randomArrayShuffle(videoartarrold)
-    if(articlearr.length == 0) {
-      combinedarr = [...videoartarr];
-    }
-    articlearr.map((x: any, index: number) => {
-      if (i < maxArticleSize) {
-        combinedarr.push(x);
-        i++;
-        if (index == articlearr.length - 1) {
-          if (j < videoartarr.length) {
-            const dd = videoartarr.splice(j);
-            dd.map((y: any) => combinedarr.push(y));
-          }
-        }
-      } else {
-        i = 1;
-        if (videoartarr[j]) { combinedarr.push(videoartarr[j]) }
-        combinedarr.push(x);
-        j++;
-        if (index == articlearr.length - 1) {
-          if (j < videoartarr.length) {
-            const dd = videoartarr.splice(j);
-            dd.map((y: any) => combinedarr.push(y));
-          }
+ //merge array 
+ const mergearr = (articlearrold: any[], videoartarrold: any[], isSuffle: boolean): any => {
+  let combinedarr: any[] = [];
+  let i = 0;
+  let j = 0;
+  let articlearr: any[] = [];
+  let videoartarr: any[] = [];
+
+  if (isSuffle) {
+    articlearr = randomArrayShuffle(articlearrold)
+    videoartarr = randomArrayShuffle(videoartarrold)
+  } else {
+    articlearr = articlearrold
+    videoartarr = videoartarrold
+  }
+
+  if (articlearr.length == 0) {
+    combinedarr = [...videoartarr];
+  }
+  articlearr.map((x: any, index: number) => {
+    if (i < maxArticleSize) {
+      combinedarr.push(x);
+      i++;
+      if (index == articlearr.length - 1) {
+        if (j < videoartarr.length) {
+          const dd = videoartarr.splice(j);
+          dd.map((y: any) => combinedarr.push(y));
         }
       }
-    });
-    
-    return combinedarr;
-  }
+    } else {
+      i = 1;
+      if (videoartarr[j]) { combinedarr.push(videoartarr[j]) }
+      combinedarr.push(x);
+      j++;
+      if (index == articlearr.length - 1) {
+        if (j < videoartarr.length) {
+          const dd = videoartarr.splice(j);
+          dd.map((y: any) => combinedarr.push(y));
+        }
+      }
+    }
+  });
+
+  return combinedarr;
+}
+
   useFocusEffect(() => {
     setModalVisible(articleModalOpened);
   })
@@ -156,26 +167,27 @@ const Articles = ({ route, navigation }: any): any => {
   );
   const videoarticleData = VideoArticlesDataall.filter((x: any) => x.mandatory == videoArticleMandatory && x.child_age.includes(activeChild.taxonomyData.id) && articleCategoryArray.includes(x.category) && (x.child_gender == activeChild?.gender || x.child_gender == bothChildGender));
 
-  let articleData: any = mergearr(articleDataOld,videoarticleData);
+  let articleData: any = mergearr(articleDataOld, videoarticleData, true);
   const [filteredData, setfilteredData] = useState<any>([]);
   const [filterArray, setFilterArray] = useState([]);
   const [loadingArticle, setLoadingArticle] = useState(true);
   const [keyboardStatus, setKeyboardStatus] = useState<any>();
   const videoIsFocused = useIsFocused();
-  const goToArticleDetail = (item: any): any => {
+  const goToArticleDetail = (item: any, queryText: string): any => {
     navigation.navigate('DetailsScreen',
       {
         fromScreen: "Articles",
         headerColor: headerColor,
         backgroundColor: backgroundColor,
         detailData: item,
-        listCategoryArray: filterArray
+        listCategoryArray: filterArray,
+        queryText: queryText
       });
   };
   const RenderArticleItem = ({ item, index }: any): any => {
     return (
     <ArticleListContainer>
-      <Pressable onPress={(): any => { goToArticleDetail(item) }} key={index}>
+      <Pressable onPress={(): any => { goToArticleDetail(item, queryText) }} key={index}>
         {(netInfoval.isConnected == true && item && item.cover_video && item.cover_video.url!="" && item.cover_video.url!=undefined) ? 
            videoIsFocused==true?<VideoPlayer selectedPinnedArticleData={item}></VideoPlayer>:null
           : <LoadableImage style={styles.cardImage} item={item} toggleSwitchVal={toggleSwitchVal} resizeMode={FastImage.resizeMode.cover} />
@@ -197,20 +209,36 @@ const Articles = ({ route, navigation }: any): any => {
     flatListRef?.current?.scrollToOffset({ animated: Platform.OS == "android" ? true : false, offset: 0 })
   }
   const setFilteredArticleData = (itemId: any): any => {
-    
     if (articleData != null && articleData != undefined && articleData.length > 0) {
       setLoadingArticle(true);
       if (itemId.length > 0) {
         let newArticleData = articleDataOld.filter((x: any) => itemId.includes(x.category));
         let newvideoArticleData = videoarticleData.filter((x: any) => itemId.includes(x.category));
+        let titleData = [];
+        let bodyData = [];
+        let videoTitleData = [];
+        let videoBodyData = [];
         if (queryText != "" && queryText != undefined && queryText != null) {
-          newArticleData = newArticleData.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.title.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
-          newvideoArticleData = newvideoArticleData.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.title.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+          // filter list for title first and then after from summary or body for article
+          titleData = newArticleData.filter((element: any) => element.title.toLowerCase().includes(queryText.toLowerCase()));
+          bodyData = newArticleData.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+          // combine array for article
+          const combineArticleData: any[] = titleData.concat(bodyData)
+          newArticleData = [...new Set(combineArticleData)];
+
+          // filter list for title first and then after from summary or body for video article
+          videoTitleData = newvideoArticleData.filter((element: any) => element.title.toLowerCase().includes(queryText.toLowerCase()));
+          videoBodyData = newvideoArticleData.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+          // combine array for video article
+          const combineVideoArticleData: any[] = videoTitleData.concat(videoBodyData)
+          newvideoArticleData = [...new Set(combineVideoArticleData)];
         }
-        const combinedartarr = mergearr(newArticleData,newvideoArticleData);
+
+        //combine-array
+        const combinedartarr = mergearr(newArticleData, newvideoArticleData, false);
 
         setfilteredData(combinedartarr);
-        
+
         setLoadingArticle(false);
         toTop();
       } else {
@@ -218,12 +246,24 @@ const Articles = ({ route, navigation }: any): any => {
         const videoarticleDataAllCategory = VideoArticlesDataall.filter((x: any) => x.mandatory == videoArticleMandatory && x.child_age.includes(activeChild.taxonomyData.id) && (x.child_gender == activeChild?.gender || x.child_gender == bothChildGender));
         let newvideoArticleData = videoarticleData != '' ? videoarticleData : [];
         let combinedartarr = [];
+        let titleData = [];
+        let bodyData = [];
+        let videoTitleData = [];
+        let videoBodyData = [];
         if (queryText != "" && queryText != undefined && queryText != null) {
-          newArticleData = articleDataall.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.title.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
-          newvideoArticleData = videoarticleDataAllCategory.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.title.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
-          combinedartarr = mergearr(newArticleData,newvideoArticleData);
+          titleData = articleDataall.filter((element: any) => element.title.toLowerCase().includes(queryText.toLowerCase()));
+          bodyData = articleDataall.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+          const combineArticleData: any[] = titleData.concat(bodyData)
+          newArticleData = [...new Set(combineArticleData)];
+
+          videoTitleData = newvideoArticleData.filter((element: any) => element.title.toLowerCase().includes(queryText.toLowerCase()));
+          videoBodyData = newvideoArticleData.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+          const combineVideoArticleData: any[] = videoTitleData.concat(videoBodyData)
+          newvideoArticleData = [...new Set(combineVideoArticleData)];
+
+          combinedartarr = mergearr(newArticleData, newvideoArticleData, false);
           setfilteredData(combinedartarr);
-        }else {
+        } else {
           setfilteredData(newArticleData);
         }
 
@@ -235,6 +275,7 @@ const Articles = ({ route, navigation }: any): any => {
       setfilteredData([]);
     }
   }
+
   useFocusEffect(
     React.useCallback(() => {
       async function fetchData(): Promise<any> {
@@ -283,30 +324,41 @@ const Articles = ({ route, navigation }: any): any => {
    
     setFilterArray(newFilterArray)
   }
-  //code for getting article dynamic data ends here.
+ //code for getting article dynamic data ends here.
   const searchList = async (queryText: any): Promise<any> => {
     setLoadingArticle(true);
-    
+
     let artData: any;
     let newvideoArticleData: any;
     let combinedartarr = [];
     const videoarticleDataAllCategory = VideoArticlesDataall.filter((x: any) => x.mandatory == videoArticleMandatory && x.child_age.includes(activeChild.taxonomyData.id) && (x.child_gender == activeChild?.gender || x.child_gender == bothChildGender));
-
+    let searchTitleData = [];
+    let searchBodyData = [];
+    let searchVideoTitleData = [];
+    let searchVideoBodyData = [];
     if (queryText != "" && queryText != undefined && queryText != null) {
-      artData = articleDataall.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.title.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
-      newvideoArticleData = videoarticleDataAllCategory.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.title.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
-      combinedartarr = mergearr(artData,newvideoArticleData);
+      searchTitleData = articleDataall.filter((element: any) => element.title.toLowerCase().includes(queryText.toLowerCase()));
+      searchBodyData = articleDataall.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+      const searchArticleData: any[] = searchTitleData.concat(searchBodyData)
+      artData = [...new Set(searchArticleData)];
+
+      searchVideoTitleData = videoarticleDataAllCategory.filter((element: any) => element.title.toLowerCase().includes(queryText.toLowerCase()));
+      searchVideoBodyData = videoarticleDataAllCategory.filter((element: any) => element.body.toLowerCase().includes(queryText.toLowerCase()) || element.summary.toLowerCase().includes(queryText.toLowerCase()));
+      const searchVideoArticleData: any[] = searchVideoTitleData.concat(searchVideoBodyData)
+      newvideoArticleData = [...new Set(searchVideoArticleData)];
+
+      combinedartarr = mergearr(artData, newvideoArticleData, false);
     }
     else {
       artData = articleDataall.filter((x: any) => articleCategoryArray.includes(x.category));
       newvideoArticleData = VideoArticlesDataall.filter((x: any) => x.mandatory == videoArticleMandatory && x.child_age.includes(activeChild.taxonomyData.id) && articleCategoryArray.includes(x.category) && (x.child_gender == activeChild?.gender || x.child_gender == bothChildGender));
-      combinedartarr = mergearr(artData,newvideoArticleData);
+      combinedartarr = mergearr(artData, newvideoArticleData, true);
       // mergearr
     }
-    
+
     articleData = [...combinedartarr];
-    
     setFilteredArticleData(filterArray);
+
   }
   return (
     <>
