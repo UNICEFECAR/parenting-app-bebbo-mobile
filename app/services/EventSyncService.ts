7773921c960ab9bbd52323271fbd3d3ada4aff8f
DivyaniPatel96 <divyani.patel@datamatics.com>
2023-08-29 18:52:00 +0530

    remove log from EventSyncService

--- app/services/EventSyncService.ts
+++ app/services/EventSyncService.ts
@@ -3,19 +3,14 @@ import analytics from '@react-native-firebase/analytics';
 
 
 export const logEventToFirebase = async (analyticsInstance: any, event: any): Promise<void> => {
-  console.log('EventClick for name', event.name);
-  console.log('EventClick for params', event.params);
   if(event.params!=undefined){
   analyticsInstance.logEvent(event.name, event.params);
   }else{
-  console.log('EventClick for name1', event.name)
     analyticsInstance.logEvent(event.name)
   }
 }
 
 export const markEventAsSynchronized = async (realm: any, event: any): Promise<void> => {
-  console.log('relm is realm',dataRealmCommon.isRealmClosed())
-  console.log('relm is event',event)
   if (!dataRealmCommon.isRealmClosed()) {
     realm = await dataRealmCommon.openRealm();
   }
@@ -26,8 +21,6 @@ export const markEventAsSynchronized = async (realm: any, event: any): Promise<v
 }
 
 export const storeUnsyncedEvent = async (realm: any, eventData: any): Promise<any> => {
-  console.log('offline store', eventData);
-  console.log('database store', dataRealmCommon.isRealmClosed());
   realm.write(() => {
     const unsyncedEvent = realm.create('Event', {
       ...eventData,
@@ -35,38 +28,29 @@ export const storeUnsyncedEvent = async (realm: any, eventData: any): Promise<an
     });
     console.log('EventClick unsyncedEvent for category', unsyncedEvent);
   });
-  //dataRealmCommon.closeRealm();
 }
 
 export const logEvent = async (eventData: any, netInfo: boolean): Promise<any> => {
-  console.log('EventClick for category online..............', netInfo);
   let realm: any;
-  console.log('EventClick for category online', dataRealmCommon.isRealmClosed());
   if (!dataRealmCommon.isRealmClosed()) {
     realm = await dataRealmCommon.openRealm();
   }
   if (netInfo == true) {
     const analyticsInstance = analytics();
     logEventToFirebase(analyticsInstance, eventData);
-    console.log('EventClick for category online');
-    console.log('EventClick for category eventData', eventData);
   } else {
     storeUnsyncedEvent(realm, eventData);
   }
 }
 
 export const synchronizeEvents = async (netInfo: boolean): Promise<any> => {
-  console.log('check for user online......',dataRealmCommon.isRealmClosed())
   let realm: any;
   if (!dataRealmCommon.isRealmClosed()) {
     realm = await dataRealmCommon.openRealm();
   }
   if (realm != null) {
     const unsynchronizedEvents = realm.objects('Event').filtered('isSynchronized = false');
-    console.log('unsynchronizedEvents', unsynchronizedEvents)
-    console.log('unsynchronizedEvents length', unsynchronizedEvents.length)
     if (!unsynchronizedEvents.length) {
-      console.log('No unsynchronized events to sync.');
       return;
     }
 
@@ -80,7 +64,5 @@ export const synchronizeEvents = async (netInfo: boolean): Promise<any> => {
     } else {
       console.log('Device is offline. Events not synced.');
     }
-  }else{
-    console.log('This is Error....')
   }
 }
