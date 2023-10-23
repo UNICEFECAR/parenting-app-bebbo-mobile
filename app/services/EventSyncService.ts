6804120021f03ac666c541f20d04aff93272b698
DivyaniPatel96 <divyani.patel@datamatics.com>
2023-10-23 11:25:59 +0530

    Remove Add brother and sister link from header slide down panel
    Remove Add brother and sister link from header slide down panel and change english string file

--- app/services/EventSyncService.ts
+++ app/services/EventSyncService.ts
@@ -54,8 +54,10 @@ export const synchronizeEvents = async (netInfo: boolean): Promise<any> => {
       const analyticsInstance = analytics();
 
       unsynchronizedEvents.forEach(async (event: any) => {
+        if (event != undefined) {
         logEventToFirebase(analyticsInstance, event);
         markEventAsSynchronized(realm, event);
+        }
       });
     } else {
       console.log('Device is offline. Events not synced.');
