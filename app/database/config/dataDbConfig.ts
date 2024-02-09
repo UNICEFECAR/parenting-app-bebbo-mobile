16b1e1be584f7fecf9feb1ad79fc150647f14640
DivyaniPatel96 <divyani.patel@datamatics.com>
2024-02-09 11:52:19 +0530

    Store and Display search results
    Implement store and display search results and resolve image issue for details and about us screen

--- app/database/config/dataDbConfig.ts
+++ app/database/config/dataDbConfig.ts
@@ -1,4 +1,5 @@
-import Realm from "realm";
+import Realm from 'realm';
+
 import { migrateConfigSettings } from "../migration/init";
 import { ActivitiesEntitySchema } from "../schema/ActivitiesSchema";
 import { ArticleEntitySchema, CoverImage, CoverVideo } from "../schema/ArticleSchema";
@@ -16,6 +17,7 @@ import { TaxonomySchema } from "../schema/TaxonomySchema";
 import { VaccinationSchema } from "../schema/VaccinationSchema";
 import { VideoArticleEntitySchema } from "../schema/VideoArticleSchema";
 import { EventSchema } from "../schema/EventSchema";
+import { SearchHistorySchema } from "../schema/SearchHistorySchema";
 
 export const dataRealmConfig: Realm.Configuration = {
   path: 'data.realm',
@@ -38,9 +40,10 @@ export const dataRealmConfig: Realm.Configuration = {
     StandDevObj,
     StandardDevHeightForAgeSchema,
     FAQsSchema,
-    EventSchema
+    EventSchema,
+    SearchHistorySchema
   ],
-  schemaVersion: 8,
+  schemaVersion: 9,
   onMigration: (oldRealm, newRealm) => {
     if (oldRealm.schemaVersion < 1) {
       // const oldObjects = oldRealm.objects('VariableEntity');
