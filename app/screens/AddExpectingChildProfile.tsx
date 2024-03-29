7bff33b00dba49506e189e68dfed8965e7d82a4d
DivyaniPatel96 <divyani.patel@datamatics.com>
2024-03-29 21:38:24 +0530

    Feat - Add skip button and manage profile changes
    Work on add skip button and manage profile design changes

--- app/screens/AddExpectingChildProfile.tsx
+++ app/screens/AddExpectingChildProfile.tsx
@@ -138,7 +138,7 @@ const AddExpectingChildProfile = ({ route, navigation }: Props): any => {
     const insertData: any = editScreen ? await getNewChild(childData?.uuid, "true", null, '', plannedTermDate, name, '', '', childData?.createdAt) : await getNewChild('', "true", null, '', plannedTermDate, name, '', '', null);
     const childSet: Array<any> = [];
     childSet.push(insertData);
-    addChild(languageCode, editScreen, 2, childSet, dispatch, navigation, childAge, null, null, netInfo);
+    addChild(languageCode, editScreen, 2, childSet, dispatch, navigation, childAge, null, null, netInfo,false);
   }
   const deleteRecord = (index: number, dispatch: any, uuid: string, childList: any): any => {
     return new Promise((resolve, reject) => {
