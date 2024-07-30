174ba19f9c9b4b0c5fcea4d61ae3a9cf14252861
Perrentbuddy <perrentbuddy@Mac-mini-2.local>
2024-07-30 19:00:17 +0530

    Reverted the package and metro config file to address the SVG transfer vector issue and fixed the dynamic country language code issue

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -9,23 +7,21 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * @type {import('metro-config').MetroConfig}
  */
 
-const blacklist = require('metro-config/src/defaults/exclusionList');
+const blacklist = require('metro-config/src/defaults/exclusionList')
 const config = {
-  resetCache: true,
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer/react-native'),
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
+    resetCache:true,
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
     blacklistRE: blacklist([/xk\/.*/]),
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
-  },
-};
+    }
+  };
+  
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
