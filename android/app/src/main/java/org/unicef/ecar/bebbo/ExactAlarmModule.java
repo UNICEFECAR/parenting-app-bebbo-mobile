f566d8e68141b58fd2511fc6b95d7dd3d8fbb389
ShahidJamal Ansari <ansari.amanulla@datamatics.com>
2024-10-07 19:57:33 +0530

    - fix the schedule exact alarm permission

---
+++ android/app/src/main/java/org/unicef/ecar/bebbo/ExactAlarmModule.java
@@ -0,0 +1,57 @@
+package org.unicef.ecar.bebbo;
+
+import android.app.AlarmManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import androidx.annotation.NonNull;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+
+public class ExactAlarmModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    ExactAlarmModule(ReactApplicationContext context) {
+        super(context);
+        this.reactContext = context;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "ExactAlarmModule";
+    }
+
+    @ReactMethod
+    public void requestExactAlarmPermission(Promise promise) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {  // Android 13 and above
+            AlarmManager alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
+            if (alarmManager.canScheduleExactAlarms()) {
+                promise.resolve(true); // Permission already granted
+            } else {
+                // Redirect user to system settings to enable SCHEDULE_EXACT_ALARM permission
+                Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                reactContext.startActivity(intent); // Open settings to request permission
+                promise.resolve(false); // Redirecting user to enable the permission
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {  // Android 12
+            AlarmManager alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
+            if (alarmManager.canScheduleExactAlarms()) {
+                promise.resolve(true); // Permission already granted
+            } else {
+                // For Android 12, request permission similarly
+                Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                reactContext.startActivity(intent); // Open settings to request permission
+                promise.resolve(false); // Redirecting user to enable the permission
+            }
+        } else {
+            promise.resolve(true); // Permission not needed for versions below Android 12
+        }
+    }
+}
