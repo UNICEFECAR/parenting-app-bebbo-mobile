0733aa584d757bae76902a1bbba641fa9c24d454
ShahidJamal Ansari <ansari.amanulla@datamatics.com>
2025-05-17 18:46:27 +0530

    - change share url for wawamor

--- app/instances/wawamor/config/appConfig.ts
+++ app/instances/wawamor/config/appConfig.ts
@@ -1,47 +1,47 @@
-import { Platform } from 'react-native';
-import { apiUrlDevelop } from 'react-native-dotenv';
-import RNFS from 'react-native-fs';
+import { Platform } from "react-native";
+import { apiUrlDevelop } from "react-native-dotenv";
+import RNFS from "react-native-fs";
 const config = {
   destinationFolder: `${RNFS.DocumentDirectoryPath}/content/`,
-  buildForBebbo: 'wawamor',
-  buildFor: 'wawamor', // 'wawamor'
+  buildForBebbo: "wawamor",
+  buildFor: "wawamor", // 'wawamor'
   maxRelatedArticleSize: 3,
-  isArticlePinned: '1',
-  articleCategory: '4,1,55,56,3,2',
+  isArticlePinned: "1",
+  articleCategory: "4,1,55,56,3,2",
   articleCategoryIdArray: [386, 396, 401, 406, 411, 416],
   articleCategoryArray: [
-    'health_and_wellbeing',
-    'nutrition_and_breastfeeding',
-    'parenting_corner',
-    'play_and_learning',
-    'responsive_parenting',
-    'safety_and_protection',
+    "health_and_wellbeing",
+    "nutrition_and_breastfeeding",
+    "parenting_corner",
+    "play_and_learning",
+    "responsive_parenting",
+    "safety_and_protection",
   ],
   activityCategoryArray: [
-    'socio_emotional',
-    'language_and_communication',
-    'cognitive',
-    'motor',
+    "socio_emotional",
+    "language_and_communication",
+    "cognitive",
+    "motor",
   ],
   regexpEmojiPresentation: /[^\p{L} ]/gu,
-  luxonDefaultLocale: 'en-US',
-  videoTypeVimeo: 'vimeo',
-  videoTypeYoutube: 'youtube',
-  videoTypeImage: 'novideo',
-  backupGDriveFolderName: 'Wawamor App Backup',
-  backupGDriveFileName: 'mybackup.json',
+  luxonDefaultLocale: "en-US",
+  videoTypeVimeo: "vimeo",
+  videoTypeYoutube: "youtube",
+  videoTypeImage: "novideo",
+  backupGDriveFolderName: "Wawamor App Backup",
+  backupGDriveFileName: "mybackup.json",
   tempRealmFile: `${RNFS.DocumentDirectoryPath}/user1.realm`,
   tempFuseJsonPath: `${RNFS.DocumentDirectoryPath}/fuse-index.json`,
   backUpPath: `${RNFS.DocumentDirectoryPath}/mybackup.json`,
   tempbackUpPath: `${RNFS.TemporaryDirectoryPath}mybackup.json`,
   firstPeriodicSyncDays: 7,
   secondPeriodicSyncDays: 30,
-  shareText: `\nhttps://www.wawamor.app/share/`,
-  shareTextButton: 'https://www.wawamor.app/share/',
+  shareText: `\nhttps://www.wawamor.ec/share/`,
+  shareTextButton: "https://www.wawamor.ec/share/",
   maleData: {
     id: 6821,
-    name: 'Male',
-    unique_name: 'male',
+    name: "Male",
+    unique_name: "male",
   },
   relationShipMotherId: 6836,
   relationShipFatherId: 6841,
@@ -49,61 +49,93 @@ const config = {
   relationShipServiceProviderId: 6851,
   femaleData: {
     id: 6826,
-    name: 'Female',
-    unique_name: 'female',
+    name: "Female",
+    unique_name: "female",
   },
   childGenderUniqueName: {
-    bothChildGender: 'both',
-    girlChildGender: 'girl',
-    boyChildGender: 'boy',
+    bothChildGender: "both",
+    girlChildGender: "girl",
+    boyChildGender: "boy",
   },
   basicPagesUniqueName: {
-    aboutus: 'about_us',
-    terms: 'terms_and_conditions',
-    privacypolicy: 'privacy_policy',
-
+    aboutus: "about_us",
+    terms: "terms_and_conditions",
+    privacypolicy: "privacy_policy",
   },
   relationshipUniqueName: {
-    relationShipMotherId: 'mother',
-    relationShipFatherId: 'father',
-    relationShipOtherCaregiverId: 'other_caregiver',
-    relationShipServiceProviderId: 'service_provider',
+    relationShipMotherId: "mother",
+    relationShipFatherId: "father",
+    relationShipOtherCaregiverId: "other_caregiver",
+    relationShipServiceProviderId: "service_provider",
   },
   parentGenderUniqueName: {
-    bothParentGender: 'both',
-    maleParentGender: 'male',
-    femaleParentGender: 'female',
+    bothParentGender: "both",
+    maleParentGender: "male",
+    femaleParentGender: "female",
   },
   articleCategoryUniqueNameObj: [
-    { name: 'playingAndLearning', id: 'play_and_learning', image: 'ic_artl_play' },
-    { name: 'healthAndWellbeingid', id: 'health_and_wellbeing', image: 'ic_artl_health' },
-    { name: 'safetyAndProtection', id: 'safety_and_protection', image: 'ic_artl_safety' },
-    { name: 'responsiveParenting', id: 'responsive_parenting', image: 'ic_artl_responsive' },
-    { name: 'parentingCorner', id: 'parenting_corner', image: 'ic_artl_parenting' },
-    { name: 'nutritionAndBreastfeeding', id: 'nutrition_and_breastfeeding', image: 'ic_artl_nutrition' },
+    {
+      name: "playingAndLearning",
+      id: "play_and_learning",
+      image: "ic_artl_play",
+    },
+    {
+      name: "healthAndWellbeingid",
+      id: "health_and_wellbeing",
+      image: "ic_artl_health",
+    },
+    {
+      name: "safetyAndProtection",
+      id: "safety_and_protection",
+      image: "ic_artl_safety",
+    },
+    {
+      name: "responsiveParenting",
+      id: "responsive_parenting",
+      image: "ic_artl_responsive",
+    },
+    {
+      name: "parentingCorner",
+      id: "parenting_corner",
+      image: "ic_artl_parenting",
+    },
+    {
+      name: "nutritionAndBreastfeeding",
+      id: "nutrition_and_breastfeeding",
+      image: "ic_artl_nutrition",
+    },
   ],
   activityCategoryUniqueNameObj: [
-    { name: 'Socio-emotional', id: 'socio_emotional', image: 'ic_act_emotional' },
-    { name: 'Language and communication', id: 'language_and_communication', image: 'ic_act_language' },
-    { name: 'Cognitive', id: 'cognitive', image: 'ic_act_cognitive' },
-    { name: 'Motor', id: 'motor', image: 'ic_act_movement' },
+    {
+      name: "Socio-emotional",
+      id: "socio_emotional",
+      image: "ic_act_emotional",
+    },
+    {
+      name: "Language and communication",
+      id: "language_and_communication",
+      image: "ic_act_language",
+    },
+    { name: "Cognitive", id: "cognitive", image: "ic_act_cognitive" },
+    { name: "Motor", id: "motor", image: "ic_act_movement" },
   ],
   activityCategoryobj: [
-    { name: 'Socio-emotional', id: 676, image: 'ic_act_emotional' },
-    { name: 'Language and communication', id: 686, image: 'ic_act_language' },
-    { name: 'Cognitive', id: 681, image: 'ic_act_cognitive' },
-    { name: 'Motor', id: 671, image: 'ic_act_movement' },
+    { name: "Socio-emotional", id: 676, image: "ic_act_emotional" },
+    { name: "Language and communication", id: 686, image: "ic_act_language" },
+    { name: "Cognitive", id: 681, image: "ic_act_cognitive" },
+    { name: "Motor", id: 671, image: "ic_act_movement" },
   ],
   articleCategoryobj: [
-    { name: 'playingAndLearning', id: 406, image: 'ic_artl_play' },
-    { name: 'healthAndWellbeingid', id: 386, image: 'ic_artl_health' },
-    { name: 'safetyAndProtection', id: 416, image: 'ic_artl_safety' },
-    { name: 'responsiveParenting', id: 411, image: 'ic_artl_responsive' },
-    { name: 'parentingCorner', id: 401, image: 'ic_artl_parenting' },
-    { name: 'nutritionAndBreastfeeding', id: 396, image: 'ic_artl_nutrition' },
+    { name: "playingAndLearning", id: 406, image: "ic_artl_play" },
+    { name: "healthAndWellbeingid", id: 386, image: "ic_artl_health" },
+    { name: "safetyAndProtection", id: 416, image: "ic_artl_safety" },
+    { name: "responsiveParenting", id: 411, image: "ic_artl_responsive" },
+    { name: "parentingCorner", id: 401, image: "ic_artl_parenting" },
+    { name: "nutritionAndBreastfeeding", id: 396, image: "ic_artl_nutrition" },
   ],
-  reviewURL:Platform.select({
-    android: "https://play.google.com/store/apps/details?id=org.unicef.ec.wawamor",
+  reviewURL: Platform.select({
+    android:
+      "https://play.google.com/store/apps/details?id=org.unicef.ec.wawamor",
     ios: "itms://itunes.apple.com/xk/app/apple-store/id6742478153?action=write-review",
   }),
   bothParentGender: 6831,
@@ -112,34 +144,34 @@ const config = {
   girlChildGender: 661,
   weightForHeight: 736,
   heightForAge: 731,
-  pregnancyId:166191,
-  languageCode:"en",
-  searchMinimumLength:3,
+  pregnancyId: 166191,
+  languageCode: "en",
+  searchMinimumLength: 3,
   today: new Date(),
   restOfTheWorldCountryId: 126,
   videoArticleMandatory: 0,
   maxArticleSize: 5,
-  flavorName: 'wawamor',
+  flavorName: "wawamor",
   apiConfig: {
-    articles: 'articles',
-    videoArticles: 'video-articles',
-    dailyMessages: 'daily-homescreen-messages',
-    basicPages: 'basic-pages',
+    articles: "articles",
+    videoArticles: "video-articles",
+    dailyMessages: "daily-homescreen-messages",
+    basicPages: "basic-pages",
     // sponsors: 'sponsors',
-    taxonomies: 'taxonomies',
-    standardDeviation: 'standard_deviation',
-    milestones: 'milestones',
-    activities: 'activities',
-    surveys: 'surveys',
-    childDevelopmentData: 'child-development-data',
-    childGrowthData: 'child-growth-data',
-    vaccinations: 'vaccinations',
-    healthCheckupData: 'health-checkup-data',
+    taxonomies: "taxonomies",
+    standardDeviation: "standard_deviation",
+    milestones: "milestones",
+    activities: "activities",
+    surveys: "surveys",
+    childDevelopmentData: "child-development-data",
+    childGrowthData: "child-growth-data",
+    vaccinations: "vaccinations",
+    healthCheckupData: "health-checkup-data",
     // pinnedContent: 'pinned-contents',
-    checkUpdate: 'check-update',
-    faqs: 'faqs',
-    archive: 'archive',
-    countryGroups: 'country-groups',
+    checkUpdate: "check-update",
+    faqs: "faqs",
+    archive: "archive",
+    countryGroups: "country-groups",
   },
   measurementPlaces: (items: any) => [
     { id: 0, title: items[0] },
@@ -173,70 +205,96 @@ const config = {
   },
   allApisObject: (isDatetimeReq: any, dateTimeObj: any): any => {
     const allApiObject = [
-      { apiEndpoint: config.apiConfig.articles, method: 'get', postdata: {}, saveinDB: true },
-      { apiEndpoint: config.apiConfig.countryGroups, method: 'get', postdata: {}, saveinDB: true },
-      { apiEndpoint: config.apiConfig.taxonomies, method: 'get', postdata: {}, saveinDB: true },
-      { apiEndpoint: config.apiConfig.basicPages, method: 'get', postdata: {}, saveinDB: true },
+      {
+        apiEndpoint: config.apiConfig.articles,
+        method: "get",
+        postdata: {},
+        saveinDB: true,
+      },
+      {
+        apiEndpoint: config.apiConfig.countryGroups,
+        method: "get",
+        postdata: {},
+        saveinDB: true,
+      },
+      {
+        apiEndpoint: config.apiConfig.taxonomies,
+        method: "get",
+        postdata: {},
+        saveinDB: true,
+      },
+      {
+        apiEndpoint: config.apiConfig.basicPages,
+        method: "get",
+        postdata: {},
+        saveinDB: true,
+      },
       {
         apiEndpoint: config.apiConfig.surveys,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.milestones,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.childDevelopmentData,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.vaccinations,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.healthCheckupData,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.standardDeviation,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.dailyMessages,
-        method: 'get',
+        method: "get",
         postdata: {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.activities,
-        method: 'get',
-        postdata: isDatetimeReq == true && dateTimeObj['activitiesDatetime'] != '' ? { datetime: dateTimeObj['activitiesDatetime'] } : {},
+        method: "get",
+        postdata:
+          isDatetimeReq == true && dateTimeObj["activitiesDatetime"] != ""
+            ? { datetime: dateTimeObj["activitiesDatetime"] }
+            : {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.faqs,
-        method: 'get',
-        postdata: isDatetimeReq == true && dateTimeObj['faqsDatetime'] != '' ? { datetime: dateTimeObj['faqsDatetime'] } : {},
+        method: "get",
+        postdata:
+          isDatetimeReq == true && dateTimeObj["faqsDatetime"] != ""
+            ? { datetime: dateTimeObj["faqsDatetime"] }
+            : {},
         saveinDB: true,
       },
       {
         apiEndpoint: config.apiConfig.videoArticles,
-        method: 'get',
+        method: "get",
         postdata:
-          isDatetimeReq && dateTimeObj['videoArticlesDatetime'] !== ''
-            ? { datetime: dateTimeObj['videoArticlesDatetime'] }
+          isDatetimeReq && dateTimeObj["videoArticlesDatetime"] !== ""
+            ? { datetime: dateTimeObj["videoArticlesDatetime"] }
             : {},
         saveinDB: true,
       },
@@ -246,13 +304,13 @@ const config = {
     if (isDatetimeReq) {
       allApiObject.push({
         apiEndpoint: config.apiConfig.archive,
-        method: 'get',
+        method: "get",
         postdata:
-          dateTimeObj['archiveDatetime'] !== ''
-            ? { datetime: dateTimeObj['archiveDatetime'] }
-            : dateTimeObj['faqPinnedContentDatetime'] !== ''
-              ? { datetime: dateTimeObj['faqPinnedContentDatetime'] }
-              : {},
+          dateTimeObj["archiveDatetime"] !== ""
+            ? { datetime: dateTimeObj["archiveDatetime"] }
+            : dateTimeObj["faqPinnedContentDatetime"] !== ""
+            ? { datetime: dateTimeObj["faqPinnedContentDatetime"] }
+            : {},
         saveinDB: true,
       });
     }
