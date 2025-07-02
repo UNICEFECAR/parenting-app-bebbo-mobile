83fcc12bddece82ff0e534cce743c8e9ccbf654c
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-07-03 02:56:26 +0530

    Fix: Resolved issue #1025 Display quick links on Home screen

--- app/components/homeScreen/ChildInfo.tsx
+++ app/components/homeScreen/ChildInfo.tsx
@@ -27,6 +27,7 @@ import {
   selectChildDevData,
   selectPinnedChildDevData,
 } from "../../services/selectors";
+import QuickLinksRow from './QuickLinksRow';
 const windowWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
   flexShrink1: { flexShrink: 1 },
@@ -143,6 +144,7 @@ const ChildInfo = (props: any): any => {
               {t("homeScreenchildBtnText")}
             </ButtonUpperCaseText>
           </ButtonPrimary>
+          <QuickLinksRow />
         </MainContainer>
       ) : null}
     </>
