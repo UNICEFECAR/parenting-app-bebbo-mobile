f93eeadce7543572953df8a592078939e3800262
neharuparel <neha.thakkar89@gmail.com>
2026-01-27 18:07:12 +0530

    - Added ellipses when Header Test is long in TabScreenHeader - Added ellipses when vaccine name is long - Added ellipses and minor design changes so the arrow doesn't shift to right when text is long - Updated the Shona and Ndebele strings - Updated the review url for ReraiUmntwana in app config for iOS - Updated App versions for Bebbo, Babuni, ReraiUmntwana

--- app/components/vaccination/tabs/PreviousVaccines.tsx
+++ app/components/vaccination/tabs/PreviousVaccines.tsx
@@ -184,7 +184,7 @@ const PreviousVaccines = (props: any): any => {
               }}
             >
               <ToolsHeadingView>
-                <Heading2>{item.periodName}</Heading2>
+                <Heading2 numberOfLines={2} ellipsizeMode="tail">{item.periodName}</Heading2>
                 <Heading5>
                   {t("vaccinesTxt")}
                   {":"}
--- app/screens/CustomDrawerContent.tsx
+++ app/screens/CustomDrawerContent.tsx
@@ -93,6 +93,10 @@ const styles = StyleSheet.create({
   headingFlexShrink: {
     flexShrink: 1,
   },
+  titleContainer: {
+    flex: 3,
+    marginHorizontal: 2,
+  },
   iconStyle: {
     alignSelf: "center",
     flex: 1,
@@ -436,7 +440,7 @@ const CustomDrawerContent = ({ navigation }: any): any => {
                 <Icon name="ic_sb_tools" size={25} color="#000" />
               </OuterIconLeft15>
             </OuterIconRow>
-            <Heading4 style={styles.headingFlexShrink}>
+            <Heading4 numberOfLines={2} ellipsizeMode="tail" style={[styles.headingFlexShrink,styles.titleContainer]}>
               {t("drawerMenutoolsTxt")}
             </Heading4>
             <Icon
@@ -659,7 +663,7 @@ const CustomDrawerContent = ({ navigation }: any): any => {
               </OuterIconLeft15>
             </OuterIconRow>
             <>
-              <Heading4 style={styles.headingFlexShrink}>
+              <Heading4 numberOfLines={2} ellipsizeMode="tail" style={[styles.headingFlexShrink,styles.titleContainer]}>
                 {t("aboutBebboDrawerMenu", {
                   appName: t("homeScreenheaderTitle"),
                 })}
